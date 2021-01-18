8eb166d1ec3feaa49e0c5c637753157e102011b5
Iga <iga@entap.co.jp>
2021-01-18 17:56:21 +0900

    [add]Entap.Basic.Forms Behaviors/BindableBehavior(Bind可能なBehaviorの基底クラス)

---
+++ Source/Entap.Basic.Forms/Entap.Basic.Forms/Behaviors/BindableBehavior.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Entap.Basic.Forms
+{
+    /// <summary>
+    /// Bind可能なBehaviorの基底クラス
+    /// </summary>
+    public class BindableBehavior<T> : Behavior<T> where T : BindableObject
+    {
+        public T AssociatedObject { get; private set; }
+
+        #region NeedsDetach BindableProperty
+        public static readonly BindableProperty NeedsDetachProperty = BindableProperty.Create(
+            nameof(NeedsDetach),
+            typeof(bool),
+            typeof(BindableBehavior<T>),
+            false,
+            defaultBindingMode: BindingMode.TwoWay,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            {
+                if (bindable is BindableBehavior<T> behavior)
+                {
+                    behavior.NeedsDetach = (bool)newValue;
+                    behavior.OnNeedsDetachChanged();
+                }
+            });
+
+        public bool NeedsDetach
+        {
+            get { return (bool)GetValue(NeedsDetachProperty); }
+            set { SetValue(NeedsDetachProperty, value); }
+        }
+        #endregion
+
+        protected override void OnAttachedTo(T bindable)
+        {
+            base.OnAttachedTo(bindable);
+            AssociatedObject = bindable;
+            if (bindable.BindingContext != null)
+            {
+                BindingContext = bindable.BindingContext;
+            }
+
+            bindable.BindingContextChanged += OnBindingContextChanged;
+        }
+
+        protected override void OnDetachingFrom(T bindable)
+        {
+            bindable.BindingContextChanged -= OnBindingContextChanged;
+            AssociatedObject = null;
+            base.OnDetachingFrom(bindable);
+        }
+
+        private void OnBindingContextChanged(object sender, EventArgs e)
+        {
+            OnBindingContextChanged();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            BindingContext = AssociatedObject.BindingContext;
+        }
+
+        private void OnNeedsDetachChanged()
+        {
+            if (!NeedsDetach) return;
+
+            OnDetachingFrom(AssociatedObject);
+        }
+    }
+}
