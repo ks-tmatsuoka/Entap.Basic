46e6669bea7d2022bd8eeab6ed1272062f404979
Iga <iga@entap.co.jp>
2021-01-19 09:00:48 +0900

    [add]Enta.Basic.Forms : Core（初期化処理定義）

---
+++ Source/Entap.Basic.Forms/Entap.Basic.Forms/Core/Core.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Entap.Basic.Forms
+{
+    public static class Core
+    {
+        static Application _application;
+
+        public static void Init(Application application)
+        {
+            _application = application;
+        }
+
+        public static Application Application
+        {
+            get
+            {
+                if (_application is null)
+                    throw new Exception("Please call Entap.Basic.Forms.Core.Init() method.");
+
+                return _application;
+            }
+        }
+    }
+}
