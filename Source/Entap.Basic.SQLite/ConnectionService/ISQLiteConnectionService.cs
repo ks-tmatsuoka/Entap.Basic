d2ab0356f1288d5d79044627a86f6c07071009bf
Iga <iga@entap.co.jp>
2021-01-20 10:36:14 +0900

    [add]Entap.Basic.SQLite : SQLiteのコネクション取得処理定義（SQLiteConnectionManager, ISQLiteConnectionManager）

---
+++ Source/Entap.Basic.SQLite/ConnectionService/ISQLiteConnectionService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using SQLite;
+
+namespace Entap.Basic.SQLite
+{
+    /// <summary>
+    /// SQLiteのコネクション取得インターフェース
+    /// </summary>
+    public interface ISQLiteConnectionService
+    {
+        SQLiteConnection GetConnection();
+        SQLiteAsyncConnection GetAsyncConnection();
+    }
+    public class TableBase
+    {
+        public TableBase()
+        {
+        }
+
+        /// <summary>
+        /// Id
+        /// </summary>
+        [PrimaryKey, AutoIncrement]
+        public int AppId { get; set; }
+
+        /// <summary>
+        /// 作成日時
+        /// </summary>
+        public DateTime CreateAt { get; set; }
+
+        /// <summary>
+        /// 更新日時
+        /// </summary>
+        public DateTime UpdatedAt { get; set; }
+    }
+}
