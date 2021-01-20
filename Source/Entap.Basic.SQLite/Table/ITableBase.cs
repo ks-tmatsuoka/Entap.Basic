d1cb46623b5691d545e1dd6d89035e9fb27c80b2
Iga <iga@entap.co.jp>
2021-01-20 17:03:55 +0900

    [add]Entap.Basic.SQLite : Tableの規定クラス、インターフェース定義

---
+++ Source/Entap.Basic.SQLite/Table/ITableBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Entap.Basic.SQLite
+{
+    /// <summary>
+    /// テーブルの規定インターフェース
+    /// </summary>
+    public interface ITableBase
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int AppId { get; set; }
+
+        /// <summary>
+        /// 作成日時
+        /// </summary>
+        public DateTime CreateAt { get; set; }
+
+        /// <summary>
+        /// 更新日時
+        /// </summary>
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Source/Entap.Basic.SQLite/Table/TableBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SQLite;
+
+namespace Entap.Basic.SQLite
+{
+    public class TableBase : ITableBase
+    {
+        public TableBase()
+        {
+        }
+
+        /// <summary>
+        /// Id
+        /// </summary>
+        [PrimaryKey, AutoIncrement]
+        public int AppId { get; set; }
+
+        /// <summary>
+        /// 作成日時
+        /// </summary>
+        public DateTime CreateAt { get; set; }
+
+        /// <summary>
+        /// 更新日時
+        /// </summary>
+        public DateTime UpdatedAt { get; set; }
+    }
+}
