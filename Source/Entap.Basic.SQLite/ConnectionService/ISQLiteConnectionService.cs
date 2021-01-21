a8807e84610b5650c6a8f04a031f9d187dd5f05f
Iga <iga@entap.co.jp>
2021-01-21 10:00:47 +0900

    [fix]Entap.Basic.SQLite : ISQLiteConnectionServiceに仮定義していたTableBaseの削除

--- Source/Entap.Basic.SQLite/ConnectionService/ISQLiteConnectionService.cs
+++ Source/Entap.Basic.SQLite/ConnectionService/ISQLiteConnectionService.cs
@@ -11,26 +11,4 @@ namespace Entap.Basic.SQLite
         SQLiteConnection GetConnection();
         SQLiteAsyncConnection GetAsyncConnection();
     }
-    public class TableBase
-    {
-        public TableBase()
-        {
-        }
-
-        /// <summary>
-        /// Id
-        /// </summary>
-        [PrimaryKey, AutoIncrement]
-        public int AppId { get; set; }
-
-        /// <summary>
-        /// 作成日時
-        /// </summary>
-        public DateTime CreateAt { get; set; }
-
-        /// <summary>
-        /// 更新日時
-        /// </summary>
-        public DateTime UpdatedAt { get; set; }
-    }
 }
