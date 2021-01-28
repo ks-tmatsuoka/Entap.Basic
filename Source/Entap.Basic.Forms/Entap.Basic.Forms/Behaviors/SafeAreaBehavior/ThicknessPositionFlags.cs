de97340d5bf28456697ed39a0da0376b775920b3
Iga <iga@entap.co.jp>
2021-01-28 10:43:35 +0900

    [add]Entap.Basic.Forms : ThicknessExtentions(Thicknessの拡張メソッド)追加

---
+++ Source/Entap.Basic.Forms/Entap.Basic.Forms/Behaviors/SafeAreaBehavior/ThicknessPositionFlags.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Entap.Basic.Forms
+{
+    /// <summary>
+    /// Thicknessのポジションを指定するフラグ
+    /// </summary>
+    [System.Flags]
+    public enum ThicknessPositionFlags
+    {
+        Left = 1,
+        Top = 2,
+        Right = 4,
+        Bottom = 8
+    }
+}
---
+++ Source/Entap.Basic.Forms/Entap.Basic.Forms/Extentions/ThicknessExtentions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace Entap.Basic.Forms
+{
+    /// <summary>
+    /// Xamarin.Forms.Thicknessの拡張メソッド
+    /// </summary>
+    public static class ThicknessExtentions
+    {
+        /// <summary>
+        /// SafeAreaを適用したThicknessを取得する
+        /// </summary>
+        /// <param name="thickness">SafeAreaを適用するThickness</param>
+        /// <param name="positionFlags">SafeAreaを適用する位置</param>
+        /// <returns>SafeAreaを適用したThickness</returns>
+        public static Thickness? GetSafeAreaAppliedThickness(this Thickness thickness, ThicknessPositionFlags positionFlags)
+        {
+            if (positionFlags == 0) return null;
+            if (Device.RuntimePlatform != Device.iOS) return null;
+
+            var currentPage = PageManager.Navigation.GetCurrentPage();
+            if (currentPage is null) return null;
+
+            var safeAreaInsets = currentPage.On<iOS>().SafeAreaInsets();
+            if (safeAreaInsets.IsEmpty) return null;
+
+            var result = new Thickness();
+            if (positionFlags.HasFlag(ThicknessPositionFlags.Left))
+                result.Left = thickness.Left + safeAreaInsets.Left;
+
+            if (positionFlags.HasFlag(ThicknessPositionFlags.Top))
+                result.Top = thickness.Top + safeAreaInsets.Top;
+
+            if (positionFlags.HasFlag(ThicknessPositionFlags.Right))
+                result.Right = thickness.Right + safeAreaInsets.Right;
+
+            if (positionFlags.HasFlag(ThicknessPositionFlags.Bottom))
+                result.Bottom = thickness.Bottom + safeAreaInsets.Bottom;
+
+            return result;
+        }
+    }
+}
