c8da8ef1b453a3ae1cd4eaa7fa477d21490398f2
Iga <iga@entap.co.jp>
2021-01-29 09:26:33 +0900

    [add]Entap.Basic.Forms : SafeAreaLayoutBehavior, SafeAreaViewBehavior : Layout, ViewにSafeAreaを適用するBehavior作成

---
+++ Source/Entap.Basic.Forms/Entap.Basic.Forms/Behaviors/SafeAreaBehavior/SafeAreaLayoutBehavior.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Entap.Basic.Forms
+{
+    /// <summary>
+    /// Paddingを使用し、LayoutにSafeAreaを適用する
+    /// </summary>
+    public class SafeAreaLayoutBehavior : BindableBehavior<Layout>
+    {
+        Thickness? defaultPadding;
+        public SafeAreaLayoutBehavior()
+        {
+        }
+
+        protected override void OnAttachedTo(BindableObject bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                OnDetachingFrom(bindable);
+                return;
+            }
+
+            SetSafePadding();
+        }
+
+        #region PositionFlags BindableProperty
+        public static readonly BindableProperty PositionFlagsProperty = BindableProperty.Create(
+            nameof(PositionFlags),
+            typeof(ThicknessPositionFlags),
+            typeof(SafeAreaLayoutBehavior),
+            null,
+            defaultBindingMode: BindingMode.Default,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            {
+                if (bindable is not SafeAreaLayoutBehavior behavior) return;
+                behavior.PositionFlags = (ThicknessPositionFlags)newValue;
+                behavior.SetSafePadding();
+            });
+
+        public ThicknessPositionFlags PositionFlags
+        {
+            get { return (ThicknessPositionFlags)GetValue(PositionFlagsProperty); }
+            set { SetValue(PositionFlagsProperty, value); }
+        }
+        #endregion
+
+        void SetSafePadding()
+        {
+            if (defaultPadding is null)
+                defaultPadding = AssociatedObject.Padding;
+
+            var padding = defaultPadding.Value.GetSafeAreaAppliedThickness(PositionFlags);
+            if (padding is null) return;
+
+            AssociatedObject.Padding = padding.Value;
+        }
+    }
+}
---
+++ Source/Entap.Basic.Forms/Entap.Basic.Forms/Behaviors/SafeAreaBehavior/SafeAreaViewBehavior.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Entap.Basic.Forms
+{
+    /// <summary>
+    /// Marginを使用し、ViewにSafeAreaを適用する
+    /// </summary>
+    public class SafeAreaViewBehavior : BindableBehavior<View>
+    {
+        Thickness? defaultMargin;
+        public SafeAreaViewBehavior()
+        {
+        }
+
+        protected override void OnAttachedTo(BindableObject bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                OnDetachingFrom(bindable);
+                return;
+            }
+
+            SetSafeMargin();
+        }
+
+        #region PositionFlags BindableProperty
+        public static readonly BindableProperty PositionFlagsProperty = BindableProperty.Create(
+            nameof(PositionFlags),
+            typeof(ThicknessPositionFlags),
+            typeof(SafeAreaViewBehavior),
+            null,
+            defaultBindingMode: BindingMode.Default,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            {
+                if (bindable is not SafeAreaViewBehavior behavior) return;
+                behavior.PositionFlags = (ThicknessPositionFlags)newValue;
+                behavior.SetSafeMargin();
+            });
+
+        public ThicknessPositionFlags PositionFlags
+        {
+            get { return (ThicknessPositionFlags)GetValue(PositionFlagsProperty); }
+            set { SetValue(PositionFlagsProperty, value); }
+        }
+        #endregion
+
+        void SetSafeMargin()
+        {
+            if (defaultMargin is null)
+                defaultMargin = AssociatedObject.Margin;
+
+            var margin = defaultMargin.Value.GetSafeAreaAppliedThickness(PositionFlags);
+            if (margin is null) return;
+
+            AssociatedObject.Margin = margin.Value;
+        }
+    }
+}
