ed30bf1353ef766e743986c52dee3d529115b131
Iga <iga@entap.co.jp>
2021-01-29 17:57:31 +0900

    [add]Entap.Basic Terms/IConfirmTermsUseCase　規約の確認ページのユースケース追加

--- Source/Entap.Basic/Entap.Basic/Launch/Terms/C001/IConfirmTermsUseCase.cs
+++ Source/Entap.Basic/Entap.Basic/Launch/Terms/C001/IConfirmTermsUseCase.cs
@@ -1,7 +1,26 @@
 ﻿using System;
-namespace Entap.Basic.Launch.Terms.C001
+namespace Entap.Basic.Launch.Terms
 {
     public interface IConfirmTermsUseCase
     {
+        /// <summary>
+        /// 利用規約の確認
+        /// </summary>
+        void ConfirmTerms();
+
+        /// <summary>
+        /// プライバシーポリシーの確認
+        /// </summary>
+        void ConfirmPrivacyPolicy();
+
+        /// <summary>
+        /// 同意チェックボックスの状態変更
+        /// </summary>
+        void ChangeChecked(bool isChecked);
+
+        /// <summary>
+        /// 規約の同意
+        /// </summary>
+        void AcceptTerms();
     }
 }
