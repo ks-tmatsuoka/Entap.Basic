4e1fecad71ed9f1e091498244b0992063d2ddb00
Iga <iga@entap.co.jp>
2021-02-01 08:55:29 +0900

    [add]Entap.Basic.Launch.Terms : ConfirmTermsUseCase定義追加

---
+++ Source/Entap.Basic/Entap.Basic/Launch/Terms/C001/ConfirmTermsUseCase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Entap.Basic.Launch.Terms
+{
+    public class ConfirmTermsUseCase : IConfirmTermsUseCase
+    {
+        public ConfirmTermsUseCase()
+        {
+        }
+
+        public void ConfirmTerms()
+        {
+        }
+
+        public void ConfirmPrivacyPolicy()
+        {
+        }
+
+        public void ChangeChecked(bool isChecked)
+        {
+            System.Diagnostics.Debug.WriteLine($"ConfirmTermsUseCase.ChangeChecked() : {isChecked}");
+        }
+
+        public void AcceptTerms()
+        {
+        }
+    }
+}
