767ff23988db24ae54a4843d0544db504f4f2357
Iga <iga@entap.co.jp>
2021-02-01 09:24:03 +0900

    [update]Entap.Basic.Launch.Terms : TermsPage : コントロール仮配置、ViewModel実装

--- Source/Entap.Basic/Entap.Basic/Launch/Terms/C002/TermsPageViewModel.cs
+++ Source/Entap.Basic/Entap.Basic/Launch/Terms/C002/TermsPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Entap.Basic.Core;
 using Entap.Basic.Forms;
 using Xamarin.Forms;
 
@@ -7,8 +8,15 @@ namespace Entap.Basic.Launch.Terms
 {
     public class TermsPageViewModel : PageViewModelBase
     {
-        public TermsPageViewModel()
+        ITermsUseCase _termsUseCase;
+        public TermsPageViewModel(ITermsUseCase termsUseCase, IPageLifeCycle pageLifeCycle = null) : base(pageLifeCycle)
         {
+            _termsUseCase = termsUseCase;
         }
+
+        public Command CloseCommand => new Command(() =>
+        {
+            _termsUseCase.Close();
+        });
     }
 }
---
+++ Source/Entap.Basic/Entap.Basic/Launch/Terms/C002/TermsUseCase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Entap.Basic.Core;
+using Entap.Basic.Forms;
+
+namespace Entap.Basic.Launch.Terms
+{
+    public class TermsUseCase : ITermsUseCase
+    {
+        public TermsUseCase()
+        {
+        }
+
+        public void Close()
+        {
+            ProcessManager.Current.Invoke(async () =>
+            {
+                await PageManager.Navigation.PopModalAsync();
+            });
+        }
+    }
+}
