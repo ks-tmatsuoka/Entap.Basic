042c7d4fa9086213df2d0fde236c169ef6518317
Iga <iga@entap.co.jp>
2021-02-01 10:12:47 +0900

    [add]Entap.Basic : LoginPortalPage

---
+++ Source/Entap.Basic/Entap.Basic/Launch/Auth/LoginPortalPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Entap.Basic.Launch.Auth
+{
+    public class LoginPortalPageViewModel : PageViewModelBase
+    {
+        public LoginPortalPageViewModel()
+        {
+        }
+    }
+}
