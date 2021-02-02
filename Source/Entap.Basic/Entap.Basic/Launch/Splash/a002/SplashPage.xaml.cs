cf14c719205d6fc25a1f9e2d7f8089d4d4db0958
Iga <iga@entap.co.jp>
2021-02-02 16:27:57 +0900

    [add]Entap.Basic : SplashPage追加

---
+++ Source/Entap.Basic/Entap.Basic/Launch/Splash/a002/SplashPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Entap.Basic.Launch.Splash
+{
+    public partial class SplashPage : ContentPage
+    {
+        public SplashPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Source/Entap.Basic/Entap.Basic/Launch/Splash/a002/SplashPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Entap.Basic.Launch.Splash
+{
+    public class SplashPageViewModel : PageViewModelBase
+    {
+        public SplashPageViewModel()
+        {
+        }
+    }
+}
