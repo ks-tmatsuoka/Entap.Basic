ce497f3b74db03f29b4f5c97db8502dbc01f22e1
Iga <iga@entap.co.jp>
2021-02-02 17:25:52 +0900

    [update]Entap.Basic.Launch.Splash : SplashPage : ActivityIndicatorを仮表示、ローディング制御実装

--- Source/Entap.Basic/Entap.Basic/Launch/Splash/a002/ISplashUseCase.cs
+++ Source/Entap.Basic/Entap.Basic/Launch/Splash/a002/ISplashUseCase.cs
@@ -5,6 +5,10 @@ namespace Entap.Basic.Launch.Splash
 {
     public interface ISplashUseCase
     {
+        /// <summary>
+        /// ロード処理
+        /// </summary>
+        /// <returns>タスク</returns>
         Task LoadAsync();
     }
 }
--- Source/Entap.Basic/Entap.Basic/Launch/Splash/a002/SplashPageViewModel.cs
+++ Source/Entap.Basic/Entap.Basic/Launch/Splash/a002/SplashPageViewModel.cs
@@ -1,13 +1,29 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Entap.Basic.Forms;
 using Xamarin.Forms;
 
 namespace Entap.Basic.Launch.Splash
 {
     public class SplashPageViewModel : PageViewModelBase
     {
-        public SplashPageViewModel()
+        ISplashUseCase _splashUseCase;
+        public SplashPageViewModel(ISplashUseCase splashUseCase, IPageLifeCycle pageLifeCycle = null) : base(pageLifeCycle)
         {
+            _splashUseCase = splashUseCase;
+            IsLoading = true;
+            Task.Run(async () =>
+            {
+                await _splashUseCase.LoadAsync();
+                IsLoading = false;
+            });
         }
+
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set => SetProperty(ref _isLoading, value);
+        }
+        bool _isLoading;
     }
 }
