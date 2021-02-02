a0208e49e9aa0623f11c2319658e0c6b52756773
Iga <iga@entap.co.jp>
2021-02-02 17:22:57 +0900

    [add]Entap.Basic.Launch.Splash : ISplashUseCase, SplashUseCase

---
+++ Source/Entap.Basic/Entap.Basic/Launch/Splash/a002/SplashUseCase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Entap.Basic.Forms;
+using Entap.Basic.Launch.Guide;
+
+namespace Entap.Basic.Launch.Splash
+{
+    public class SplashUseCase : ISplashUseCase
+    {
+        public SplashUseCase()
+        {
+        }
+
+        public async Task LoadAsync()
+        {
+            await Task.Delay(3000);
+            var contents = new List<GuideContent>()
+            {
+                new GuideContent { Title = "title 1", Description = "description 1", Next = "つぎへ" },
+                new GuideContent { Title = "title 2", Description = "description 2", Next = "つぎへ" },
+                new GuideContent { Title = "title 3", Description = "description 3", Next = "はじめる" }
+            };
+            await PageManager.Navigation.SetNavigationMainPage<GuidePage>(new GuidePageViewModel(contents, new GuideUseCase()));
+        }
+    }
+}
