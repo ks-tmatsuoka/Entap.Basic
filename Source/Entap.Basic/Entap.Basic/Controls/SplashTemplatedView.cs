e1aa4c9131270c35a080cce7edf35c9e569c2f8d
Iga <iga@entap.co.jp>
2021-02-03 13:21:09 +0900

    [add]Entap.Basic : Controls.SplashTemplatedView
    a002_状態取得のローディング画面をカスタマイズ可能なTemplatedView

---
+++ Source/Entap.Basic/Entap.Basic/Controls/SplashTemplatedView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Entap.Basic.Controls
+{
+    public class SplashTemplatedView : TemplatedView
+    {
+        public SplashTemplatedView()
+        {
+        }
+
+        #region IsLoading BindableProperty
+        public static readonly BindableProperty IsLoadingProperty = BindableProperty.Create(
+            nameof(IsLoading),
+            typeof(bool),
+            typeof(SplashTemplatedView),
+            false,
+            defaultBindingMode: BindingMode.TwoWay,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            ((SplashTemplatedView)bindable).IsLoading = (bool)newValue);
+
+        public bool IsLoading
+        {
+            get { return (bool)GetValue(IsLoadingProperty); }
+            set { SetValue(IsLoadingProperty, value); }
+        }
+        #endregion
+    }
+}
