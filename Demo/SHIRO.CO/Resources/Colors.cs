30735de4b0bd09b456fa0f16d69d11a69dbcf58a
Iga <iga@entap.co.jp>
2021-02-04 17:29:21 +0900

    [add]Demo : Colors (色の定義)追加

---
+++ Demo/SHIRO.CO/Resources/Colors.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SHIRO.CO
+{
+    public class Colors
+    {
+        // BG
+        public static readonly Color BackgroundMain = Color.FromHex(Theme);
+        public static readonly Color BackgroundSub = Color.FromHex(GrayLight);
+        public static readonly Color BackgroundModal = Color.FromHex("#000000").MultiplyAlpha(20);
+
+        // Bar
+        public static readonly Color BarHeader = Color.FromHex(White);
+        public static readonly Color BarStatusBar = Color.FromHex(Theme);
+        public static readonly Color BarFooter = Color.FromHex(White);
+
+        // Button Background
+        public static readonly Color ButtonPositive = Color.FromHex(Theme);
+        public static readonly Color ButtonNeutral = Color.FromHex(White);
+        public static readonly Color ButtonNegative = Color.FromHex(White);
+        public static readonly Color ButtonDisabled = Color.FromHex(Gray);
+        // Button Border
+        public static readonly Color ButtonBorderTheme = Color.FromHex(Theme);
+        public static readonly Color ButtonBorderGray = Color.FromHex(Gray);
+
+        // Text
+        public static readonly Color TextDefault = Color.FromHex(Black);
+        public static readonly Color TextWhite = Color.FromHex(White);
+        public static readonly Color TextContrast = Color.FromHex(BlackLight);
+        public static readonly Color TextLight = Color.FromHex(GrayDark);
+        public static readonly Color TextLink = Color.FromHex(Link);
+        public static readonly Color TextCaution = Color.FromHex(Caution);
+        public static readonly Color TextMain = Color.FromHex(Theme);
+        public static readonly Color TextAccent = Color.FromHex(Accent);
+
+        const string Black = "#4A4A4A";
+        const string BlackLight = "#848384";
+        const string GrayDark = "#BABABA";
+        const string Gray = "#D8D8D8";
+        const string GrayLight = "#F7F7F7";
+        const string White = "#FFFFFF";
+        const string Link = "#007AFF";
+        const string Caution = "#FF4C4C";
+        const string Theme = "#161616";
+        const string Accent = "#40B5FF";
+    }
+}
