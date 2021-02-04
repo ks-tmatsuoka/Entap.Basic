ec70cd01093ddddbae1a88c9bf61b5a36fae563d
Iga <iga@entap.co.jp>
2021-02-04 10:29:04 +0900

    [add]Entap.Basic.Controls : GuideDataTemplateSelector定義

---
+++ Source/Entap.Basic/Entap.Basic/Controls/GuideDataTemplateSelector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Entap.Basic.Launch.Guide;
+using Xamarin.Forms;
+
+namespace Entap.Basic.Controls
+{
+    public class GuideDataTemplateSelector : DataTemplateSelector
+    {
+        public GuideDataTemplateSelector()
+        {
+        }
+
+        /// <summary>
+        /// 画像用DataTemplate
+        /// </summary>
+        public DataTemplate ImageDataTemplate { get; set; }
+
+        /// <summary>
+        /// アニメーション用DataTemplate
+        /// </summary>
+        public DataTemplate AnimationTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if (item is not GuideContent guideContent) return null;
+
+            return guideContent.ContentType switch
+            {
+                GuideContentType.Image => ImageDataTemplate,
+                GuideContentType.Animation => AnimationTemplate,
+                _ => null,
+            };
+        }
+    }
+}
