1b7b580ca552a858b59c9ace7ac4dc80004c573c
Iga <iga@entap.co.jp>
2021-02-04 14:31:25 +0900

    [update]Entap.Basic : Launch.Guide.GuidePage : コンテンツのレイアウト
    ・ItemTemplateは、GuideDataTemplateSelectorを指定
    ・GuidePage用のResourceDictionaryを追加

---
+++ Source/Entap.Basic/Entap.Basic/Launch/Guide/GuideResourceDictionary.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Entap.Basic.Launch.Guide
+{
+    public partial class GuideResourceDictionary : ResourceDictionary
+    {
+        public GuideResourceDictionary()
+        {
+            InitializeComponent();
+        }
+    }
+}
