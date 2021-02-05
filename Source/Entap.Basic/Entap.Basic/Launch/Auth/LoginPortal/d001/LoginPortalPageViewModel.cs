9284e3fdf01cb3e3d76b34991461395d4d97dcf9
Iga <iga@entap.co.jp>
2021-02-05 12:00:30 +0900

    [add]Entap.Basic : LoginPortalPage : スキップ処理仮実装、ViewModelの引数設定

--- Source/Entap.Basic/Entap.Basic/Launch/Auth/LoginPortal/d001/LoginPortalPageViewModel.cs
+++ Source/Entap.Basic/Entap.Basic/Launch/Auth/LoginPortal/d001/LoginPortalPageViewModel.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Entap.Basic.Forms;
 using Xamarin.Forms;
 
 namespace Entap.Basic.Launch.Auth
 {
     public class LoginPortalPageViewModel : PageViewModelBase
     {
-        public LoginPortalPageViewModel()
+        ILoginPortalUseCase _loginPortalUseCase;
+        public LoginPortalPageViewModel(ILoginPortalUseCase loginPortalUseCase, IPageLifeCycle pageLifeCycle = null) : base(pageLifeCycle)
         {
+            _loginPortalUseCase = loginPortalUseCase;
         }
+
+        public Command SkipCommand => new Command(() => _loginPortalUseCase.SkipAuth());
     }
 }
