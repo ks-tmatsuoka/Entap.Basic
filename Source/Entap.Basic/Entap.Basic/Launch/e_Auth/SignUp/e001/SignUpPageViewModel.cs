bc98b735732a3f67c8e4e63214e24e2d7ccce84a
Iga <iga@entap.co.jp>
2021-02-05 12:31:00 +0900

    [add]Entap.Basic : SignUpPage 追加

---
+++ Source/Entap.Basic/Entap.Basic/Launch/e_Auth/SignUp/e001/SignUpPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Entap.Basic.Forms;
+using Xamarin.Forms;
+
+namespace Entap.Basic.Launch.Auth
+{
+    public class SignUpPageViewModel : PageViewModelBase
+    {
+        public SignUpPageViewModel(IPageLifeCycle pageLifeCycle = null) : base(pageLifeCycle)
+        {
+        }
+    }
+}
