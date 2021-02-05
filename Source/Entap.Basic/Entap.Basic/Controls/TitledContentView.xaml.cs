031ecef56bc197121d00892421b1bcc3076c551a
Iga <iga@entap.co.jp>
2021-02-05 17:29:06 +0900

    [update]Entap.Basic : TitledContentView : コントロール配置、BindableProperty定義

--- Source/Entap.Basic/Entap.Basic/Controls/TitledContentView.xaml.cs
+++ Source/Entap.Basic/Entap.Basic/Controls/TitledContentView.xaml.cs
@@ -10,5 +10,90 @@ namespace Entap.Basic.Controls
         {
             InitializeComponent();
         }
+
+        #region Title BindableProperty
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create(
+            nameof(Title),
+            typeof(string),
+            typeof(TitledContentView),
+            null,
+            defaultBindingMode: BindingMode.Default,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            ((TitledContentView)bindable).Title = (string)newValue);
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+        #endregion
+
+        #region IsRequired BindableProperty
+        public static readonly BindableProperty IsRequiredProperty = BindableProperty.Create(
+            nameof(IsRequired),
+            typeof(bool),
+            typeof(TitledContentView),
+            false,
+            defaultBindingMode: BindingMode.TwoWay,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            ((TitledContentView)bindable).IsRequired = (bool)newValue);
+
+        public bool IsRequired
+        {
+            get { return (bool)GetValue(IsRequiredProperty); }
+            set { SetValue(IsRequiredProperty, value); }
+        }
+        #endregion
+
+        #region Placeholder BindableProperty
+        public static readonly BindableProperty PlaceholderProperty = BindableProperty.Create(
+            nameof(Placeholder),
+            typeof(string),
+            typeof(TitledContentView),
+            null,
+            defaultBindingMode: BindingMode.Default,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            ((TitledContentView)bindable).Placeholder = (string)newValue);
+
+        public string Placeholder
+        {
+            get { return (string)GetValue(PlaceholderProperty); }
+            set { SetValue(PlaceholderProperty, value); }
+        }
+        #endregion
+
+        #region Text BindableProperty
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(
+            nameof(Text),
+            typeof(string),
+            typeof(TitledContentView),
+            null,
+            defaultBindingMode: BindingMode.TwoWay,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            ((TitledContentView)bindable).Text = (string)newValue);
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        #endregion
+
+        #region Message BindableProperty
+        public static readonly BindableProperty MessageProperty = BindableProperty.Create(
+            nameof(Message),
+            typeof(string),
+            typeof(TitledContentView),
+            null,
+            defaultBindingMode: BindingMode.TwoWay,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            ((TitledContentView)bindable).Message = (string)newValue);
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+        #endregion
     }
 }
