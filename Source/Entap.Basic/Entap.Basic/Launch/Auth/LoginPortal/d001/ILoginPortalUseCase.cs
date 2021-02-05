0922ff09f44eebabfbe5e416727cdca37a346ab6
Iga <iga@entap.co.jp>
2021-02-05 10:59:33 +0900

    [add]Entap.Basic : ILoginPortalUseCase 定義

---
+++ Source/Entap.Basic/Entap.Basic/Launch/Auth/LoginPortal/d001/ILoginPortalUseCase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Entap.Basic.Launch.Auth
+{
+    public interface ILoginPortalUseCase
+    {
+        /// <summary>
+        /// 認証スキップ
+        /// </summary>
+        void SkipAuth();
+    }
+}
