3006839d819a34dad42ae5b6d8caaddda4f11bb1
Iga <iga@entap.co.jp>
2021-02-05 09:33:13 +0900

    [add]Entap.Basic, Demo : ButtonStyles定義（Entap.Basicは空定義）

---
+++ Source/Entap.Basic/Entap.Basic/Resources/ResourceDictionaries/ButtonStyles.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Entap.Basic.Resources
+{
+    public partial class ButtonStyles : ResourceDictionary
+    {
+        public ButtonStyles()
+        {
+            InitializeComponent();
+        }
+    }
+}
