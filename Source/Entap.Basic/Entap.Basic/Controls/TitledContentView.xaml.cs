762186f9ea5b878a0b90478c52c62f02cf388331
Iga <iga@entap.co.jp>
2021-02-05 17:22:56 +0900

    [add]Entap.Basic.TitledContentView

---
+++ Source/Entap.Basic/Entap.Basic/Controls/TitledContentView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Entap.Basic.Controls
+{
+    public partial class TitledContentView : ContentView
+    {
+        public TitledContentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
