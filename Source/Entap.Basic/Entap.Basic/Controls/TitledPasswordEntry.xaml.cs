89a8eccbc3bc61d6161a1116bbeb83c851e887ff
Iga <iga@entap.co.jp>
2021-02-08 17:03:54 +0900

    [update]TitledPasswordEntry : パスワード入力用Entry定義

---
+++ Source/Entap.Basic/Entap.Basic/Controls/TitledPasswordEntry.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Entap.Basic.Controls
+{
+    public partial class TitledPasswordEntry : TitledEntry
+    {
+        public TitledPasswordEntry()
+        {
+            InitializeComponent();
+            visibleButton.Clicked += OnVisibleButtonClicked;
+        }
+
+        void OnVisibleButtonClicked(object sender, EventArgs e)
+        {
+            IsPassword = !IsPassword;
+        }
+
+        #region IsPassword BindableProperty
+        public static readonly BindableProperty IsPasswordProperty = BindableProperty.Create(
+            nameof(IsPassword),
+            typeof(bool),
+            typeof(TitledPasswordEntry),
+            true,
+            defaultBindingMode: BindingMode.Default,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            ((TitledPasswordEntry)bindable).IsPassword = (bool)newValue);
+
+        public bool IsPassword
+        {
+            get { return (bool)GetValue(IsPasswordProperty); }
+            set { SetValue(IsPasswordProperty, value); }
+        }
+        #endregion
+    }
+}
