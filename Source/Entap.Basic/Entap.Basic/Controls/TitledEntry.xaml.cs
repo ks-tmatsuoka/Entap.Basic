f242fe9f7981e2fd5f6951e3db8660f95d229f9c
Iga <iga@entap.co.jp>
2021-02-08 15:25:34 +0900

    [add]Entap.Basic : TitledEntry

---
+++ Source/Entap.Basic/Entap.Basic/Controls/TitledEntry.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace Entap.Basic.Controls
+{
+    public partial class TitledEntry : TitledContentView
+    {
+        public TitledEntry()
+        {
+            InitializeComponent();
+
+            entry.PropertyChanged += (sender, e) =>
+            {
+                System.Diagnostics.Debug.WriteLine($"{e.PropertyName} {entry.IsFocused}");
+            };
+        }
+
+        #region Text BindableProperty
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(
+            nameof(Text),
+            typeof(string),
+            typeof(TitledEntry),
+            null,
+            defaultBindingMode: BindingMode.TwoWay,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            ((TitledEntry)bindable).Text = (string)newValue);
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        #endregion
+
+        #region Keyboard BindableProperty
+        public static readonly BindableProperty KeyboardProperty = BindableProperty.Create(
+            nameof(Keyboard),
+            typeof(Keyboard),
+            typeof(TitledEntry),
+            Keyboard.Default,
+            defaultBindingMode: BindingMode.Default,
+            propertyChanged: (bindable, oldValue, newValue) =>
+            ((TitledEntry)bindable).Keyboard = (Keyboard)newValue);
+
+        public Keyboard Keyboard
+        {
+            get { return (Keyboard)GetValue(KeyboardProperty); }
+            set { SetValue(KeyboardProperty, value); }
+        }
+        #endregion
+    }
+}
---
+++ Source/Entap.Basic/Entap.Basic/Resources/ResourceDictionaries/TitledEntryResources.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Entap.Basic.Resources
+{
+    public partial class TitledEntryResources : ResourceDictionary
+    {
+        public TitledEntryResources()
+        {
+            InitializeComponent();
+        }
+    }
+}
