349836fa26b0b60f4bbe03c17e9ef238d3e2c407
Iga <iga@entap.co.jp>
2021-02-08 15:26:32 +0900

    [add]Entap.Basic : ISignUoUseCaseの定義、SignUpUseCaseの仮実装

---
+++ Source/Entap.Basic/Entap.Basic/Launch/e_Auth/SignUp/e001/ISignUpUseCase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Entap.Basic.Launch.Auth
+{
+    public interface ISignUpUseCase
+    {
+        string ValidateMailAddress(string mailAddress);
+        string ValidatePassword(string password);
+
+        void SignUp(string mailAddress, string password);
+    }
+}
---
+++ Source/Entap.Basic/Entap.Basic/Launch/e_Auth/SignUp/e001/SignUpUseCase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Entap.Basic.Launch.Auth
+{
+    public class SignUpUseCase : ISignUpUseCase
+    {
+        public static readonly string RegexMailAddress = @"^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
+        public static readonly string RegexPassword = @"^[!-~]*$";
+
+        public SignUpUseCase()
+        {
+        }
+
+        public string ValidateMailAddress(string mailAddress)
+        {
+            if (string.IsNullOrEmpty(mailAddress)) return null;
+            if (!Regex.IsMatch(mailAddress, RegexMailAddress)) return "形式不正";
+            return null;
+        }
+
+        public string ValidatePassword(string password)
+        {
+            if (string.IsNullOrEmpty(password)) return null;
+            if (password.Length < 8) return "桁数不足";
+            if (!Regex.IsMatch(password, RegexPassword)) return "形式不正";
+
+            return null;
+        }
+
+        public void SignUp(string mailAddress, string passwrod)
+        {
+
+        }
+    }
+}
