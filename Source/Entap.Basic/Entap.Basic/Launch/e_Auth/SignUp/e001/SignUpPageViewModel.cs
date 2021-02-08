46925260455bd534ca2b83312afe1aa83c3d08bf
Iga <iga@entap.co.jp>
2021-02-08 15:27:56 +0900

    [update]Entap.Basic : SignUpPage : コントロール配置、ViewModel実装

--- Source/Entap.Basic/Entap.Basic/Launch/e_Auth/SignUp/e001/SignUpPageViewModel.cs
+++ Source/Entap.Basic/Entap.Basic/Launch/e_Auth/SignUp/e001/SignUpPageViewModel.cs
@@ -7,8 +7,57 @@ namespace Entap.Basic.Launch.Auth
 {
     public class SignUpPageViewModel : PageViewModelBase
     {
-        public SignUpPageViewModel(IPageLifeCycle pageLifeCycle = null) : base(pageLifeCycle)
+        readonly ISignUpUseCase _signUpUseCase;
+        public SignUpPageViewModel(ISignUpUseCase signUpUseCase, IPageLifeCycle pageLifeCycle = null) : base(pageLifeCycle)
         {
+            _signUpUseCase = signUpUseCase;
+            SignUpCommand = new Command(
+                () => _signUpUseCase.SignUp(MailAddress, Password),
+                () =>
+                    !string.IsNullOrEmpty(MailAddress)&&
+                    !IsMailAddressError &&
+                    !string.IsNullOrEmpty(Password) &&
+                    !IsPasswordError);  
         }
+
+        public string MailAddress
+        {
+            get => _mailAddress;
+            set
+            {
+                if (SetProperty(ref _mailAddress, value))
+                {
+                    OnPropertyChanged(nameof(MailAddressErrorMessage));
+                    OnPropertyChanged(nameof(IsMailAddressError));
+                    SignUpCommand?.ChangeCanExecute();
+                }
+            }
+        }
+        string _mailAddress;
+
+        public string MailAddressErrorMessage => _signUpUseCase.ValidateMailAddress(MailAddress);
+
+        public bool IsMailAddressError =>  !string.IsNullOrEmpty(MailAddressErrorMessage);
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (SetProperty(ref _password, value))
+                {
+                    OnPropertyChanged(nameof(PasswordErrorMessage));
+                    OnPropertyChanged(nameof(IsPasswordError));
+                    SignUpCommand?.ChangeCanExecute();
+                }
+            }
+        }
+        string _password;
+
+        public string PasswordErrorMessage => _signUpUseCase.ValidatePassword(Password);
+
+        public bool IsPasswordError => !string.IsNullOrEmpty(PasswordErrorMessage);
+
+        public Command SignUpCommand { get; set; }
     }
 }
