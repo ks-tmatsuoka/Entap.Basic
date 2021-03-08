7d9a80c530d2faa304e6c6c14baa43b11bfc6de9
Iga <iga@entap.co.jp>
2021-03-08 16:20:12 +0900

    [add]Entap.Basic.StartUp : プロバイダ、コレクション保有クラス定義

---
+++ Source/Entap.Basic/Entap.Basic/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Entap.Basic
+{
+    public static class Startup
+    {
+        public static ServiceCollection ServiceCollection { get; set; }
+        public static IServiceProvider ServiceProvider => ServiceCollection.BuildServiceProvider();
+
+        static Startup()
+        {
+            ServiceCollection = new ServiceCollection();
+        }
+    }
+}
