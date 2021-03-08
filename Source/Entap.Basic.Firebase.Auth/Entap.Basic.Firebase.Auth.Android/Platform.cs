50a58f72dbbb62e2785e3f3f39b408240255a23c
Iga <iga@entap.co.jp>
2021-03-08 09:43:40 +0900

    [add]Entap.Basic.Firebase.Auth.iOS, Android : Platfrom（初期化処理）追加

---
+++ Source/Entap.Basic.Firebase.Auth/Entap.Basic.Firebase.Auth.Android/Platform.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Firebase;
+using Plugin.CurrentActivity;
+
+namespace Entap.Basic.Firebase.Auth.Android
+{
+    [Preserve(AllMembers = true)]
+    public class Platform
+    {
+        public static void Init(Context context, Activity activity, Bundle bundle)
+        {
+            CrossCurrentActivity.Current.Init(activity, bundle);
+            FirebaseApp.InitializeApp(context);
+        }
+    }
+}
---
+++ Source/Entap.Basic.Firebase.Auth/Entap.Basic.Firebase.Auth.iOS/Platform.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Foundation;
+
+namespace Entap.Basic.Firebase.Auth.iOS
+{
+    [Preserve(AllMembers = true)]
+    public class Platform
+    {
+        public static void Init()
+        {
+            global::Firebase.Core.App.Configure();
+        }
+    }
+}
