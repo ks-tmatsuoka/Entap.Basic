8304be334181380f570bc0ed424ce5f0659441bd
Iga <iga@entap.co.jp>
2021-03-08 20:17:08 +0900

    [add]Entap.Basic.Auth : IPasswordAuthService

---
+++ Source/Entap.Basic.Auth/Entap.Basic.Auth/Interfaces/IPasswordAuthService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Entap.Basic.Auth.Abstractions
+{
+    public interface IPasswordAuthService
+    {
+        #nullable enable
+        Task<string?> SignUpAsync(string email, string password);
+        Task<string?> SignInAsync(string email, string password);
+        #nullable disable
+        void SignOut();
+    }
+}
