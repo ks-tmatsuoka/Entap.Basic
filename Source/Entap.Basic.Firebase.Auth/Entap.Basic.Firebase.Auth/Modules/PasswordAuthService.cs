297edc6af72961b510e47a1a5bf9daba125bab49
Iga <iga@entap.co.jp>
2021-03-09 08:44:46 +0900

    [update]Entap.Basic.Firebase.Auth : PasswordAuthService

---
+++ Source/Entap.Basic.Firebase.Auth/Entap.Basic.Firebase.Auth/Modules/PasswordAuthService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Entap.Basic.Auth.Abstractions;
+using Plugin.FirebaseAuth;
+
+namespace Entap.Basic.Firebase.Auth
+{
+    public class PasswordAuthService : IPasswordAuthService
+    {
+        public PasswordAuthService()
+        {
+        }
+
+
+        /// <summary>
+        /// 新しいユーザーアカウントの作成とサインインを行い、ユーザトークンを返す
+        /// </summary>
+        /// <param name="email">メールアドレス</param>
+        /// <param name="password">パスワード</param>
+        /// <returns>トークン</returns>
+        /// https://firebase.google.com/docs/reference/js/firebase.auth.Auth?hl=ja#createuserwithemailandpassword
+#nullable enable
+        public async Task<string?> SignUpAsync(string email, string password)
+#nullable disable
+        {
+            var result = await CrossFirebaseAuth.Current.Instance.CreateUserWithEmailAndPasswordAsync(email, password);
+            return await result?.User?.GetIdTokenAsync(false);
+        }
+
+        /// <summary>
+        /// メールアドレスとパスワードでサインインを行い、ユーザトークンを返す
+        /// </summary>
+        /// <param name="email">メールアドレス</param>
+        /// <param name="password">パスワード</param>
+        /// <returns></returns>
+#nullable enable
+        public async Task<string?> SignInAsync(string email, string password)
+#nullable disable
+        {
+            var result = await CrossFirebaseAuth.Current.Instance.SignInWithEmailAndPasswordAsync(email, password);
+            return await result?.User?.GetIdTokenAsync(false);
+        }
+
+        /// <summary>
+        /// サインアウト
+        /// </summary>
+        public void SignOut()
+        {
+            CrossFirebaseAuth.Current.Instance.SignOut();
+        }
+    }
+}
