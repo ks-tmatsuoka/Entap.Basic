4f01b879ebea6c8be35f2c220bea9cfb17ca5750
Iga <iga@entap.co.jp>
2021-03-09 11:47:00 +0900

    [add]Entap.Basic.Api : IAuthApi定義

---
+++ Source/Entap.Basic/Entap.Basic.Api/Interfaces/IAuthApi.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Entap.Basic.Api
+{
+    /// <summary>
+    /// 認証周りのAPI
+    /// https://github.com/entap/laravel-template/blob/main/reference/Auth.v1.yaml
+    /// </summary>
+    public interface IAuthApi
+    {
+    }
+}
