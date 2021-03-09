679a5d74ba06b8d215272962e45cb31602e8f2b8
Iga <iga@entap.co.jp>
2021-03-09 16:56:35 +0900

    [update]Entap.Basic.Api : IAuthApi : メソッド・パスのコメントの形式、宣言場所を変更

--- Source/Entap.Basic/Entap.Basic.Api/Interfaces/IAuthApi.cs
+++ Source/Entap.Basic/Entap.Basic.Api/Interfaces/IAuthApi.cs
@@ -11,70 +11,72 @@ namespace Entap.Basic.Api
     public interface IAuthApi
     {
         /// <summary>
-        /// [post]/auth/firebase/user
         /// Firebase連携を登録する
         /// ログインしているユーザーにFirebaseアカウントを紐付けます。すでに紐づいている場合は何もしません。
         /// </summary>
         /// <param name="request">リクエストパラメータ</param>
+        /// [Post("/auth/firebase/user")]
         Task PostAuthFirebaseUser(FirebaseIdToken request, CancellationToken token = default);
 
         /// <summary>
-        /// [delete]/auth/firebase/user
+        /// [Delete("/auth/firebase/user")]
         /// Firebase連携を解除する
         /// ログインしているユーザーに紐づいているFirebaseの情報を削除します。（サーバー上のみ）
         /// </summary>
+        /// [Delete("/auth/firebase/user")]
         Task DeleteAuthFirebaseUser(CancellationToken token = default);
 
         /// <summary>
-        /// [post]/auth/firebase/token
         /// アクセストークンを発行する
         /// FirebaseのIDトークンから、サーバーのアクセストークンを発行します。
         /// 該当するユーザーが見つからない場合は、新たに作成します。
         /// </summary>
         /// <param name="request">FirebaseIdToken</param>
         /// <returns>ServerAccessToken</returns>
+        /// [Post("/auth/firebase/token")]
         Task<ServerAccessToken> PostAuthFirebaseToken(FirebaseIdToken request, CancellationToken token = default);
 
         /// <summary>
-        /// [delete]/auth/line/user
         /// LINE連携を解除する
         /// ログインしているユーザーに紐づいているLINEの情報を削除します。（サーバー上のみ）
         /// </summary>
+        /// [Delete("/auth/line/user")]
         Task DeleteAuthLineUser(CancellationToken token = default);
 
         /// <summary>
-        /// [post]/auth/line/user
         /// LINE連携を登録する
         /// ログインしているユーザーにLINEのアカウントを紐付けます。
         /// すでに紐づいている場合は何もしません。
         /// </summary>
         /// <param name="request">CustomAuthToken</param>
+        /// [Post("/auth/line/user")]
         Task PostAuthLineUser(CustomAuthToken request, CancellationToken token = default);
 
         /// <summary>
-        /// [post]/auth/line/token
         /// アクセストークンを発行する
         /// LINEのIDトークンまたはアクセストークンから、サーバーのアクセストークンを発行します。
         /// 該当するユーザーが見つからない場合は、新たに作成します。
         /// </summary>
         /// <param name="request"></param>
         /// <returns>ServerAccessToken</returns>
+        /// [Post("/auth/line/token")]
         Task<ServerAccessToken> PostAuthLineToken(CustomAuthToken request, CancellationToken token = default);
 
         /// <summary>
-        /// [post]/auth/firebase/custom-token
+        /// [post]
         /// カスタムトークンを発行する
         /// Firebaseの認証に必要なカスタムトークンを発行します。
         /// </summary>
         /// <returns>FirebaseCustomToken</returns>
+        /// [Post("/auth/firebase/custom-token")]
         Task<FirebaseCustomToken> PostAuthFirebaseCustomToken(CancellationToken token = default);
 
         /// <summary>
-        /// [get]/user
         /// ユーザー情報を取得する
         /// ログインしているユーザーの情報を取得します。
         /// </summary>
         /// <returns>User</returns>
+        /// [Get("/user")]
         Task<User> GetUser(CancellationToken token = default);
     }
 }
