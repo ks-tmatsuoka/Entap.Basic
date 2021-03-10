18dad43b55725c8e61cdda9064c7ff4d30698485
Iga <iga@entap.co.jp>
2021-03-10 17:21:14 +0900

    [add]Demo : HomePage

---
+++ Demo/SHIRO.CO/Views/Pages/HomePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace SHIRO.CO
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Demo/SHIRO.CO/Views/Pages/HomePageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Entap.Basic.Forms;
+using Xamarin.Forms;
+
+namespace SHIRO.CO
+{
+    public class HomePageViewModel : PageViewModelBase
+    {
+        public HomePageViewModel()
+        {
+        }
+    }
+}
