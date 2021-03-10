bf857b0b22de3dde647e8783d65a2cec190f6e9a
Iga <iga@entap.co.jp>
2021-03-10 15:24:11 +0900

    [update]Entap.Basic : ILoginPortalUseCase : サインインの処理定義

--- Source/Entap.Basic/Entap.Basic/Launch/d_LoginPortal/d001/BasicLoginPortalUseCase.cs
+++ Source/Entap.Basic/Entap.Basic/Launch/d_LoginPortal/d001/BasicLoginPortalUseCase.cs
@@ -7,9 +7,62 @@ namespace Entap.Basic.Launch.LoginPortal
         {
         }
 
+        public virtual void SignUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        #region SignIn
         public virtual void SkipAuth()
         {
+            throw new NotImplementedException();
         }
+
+        public virtual void SignInWithEmailAndPassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void SignInWithEmailLink()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void SignInWithFacebook()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void SignInWithTwitter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void SignInWithGoogle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void SignInWithMicrosoft()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void SignInWithApple()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void SignInWithLine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void SignInWithSMS()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
 
         #region IPageLifeCycle
         public virtual void OnCreate() { }
--- Source/Entap.Basic/Entap.Basic/Launch/d_LoginPortal/d001/ILoginPortalUseCase.cs
+++ Source/Entap.Basic/Entap.Basic/Launch/d_LoginPortal/d001/ILoginPortalUseCase.cs
@@ -9,5 +9,55 @@ namespace Entap.Basic.Launch.LoginPortal
         /// 認証スキップ
         /// </summary>
         void SkipAuth();
+
+        /// <summary>
+        /// 新規登録
+        /// </summary>
+        void SignUp();
+
+        /// <summary>
+        /// パスワード認証
+        /// </summary>
+        void SignInWithEmailAndPassword();
+
+        /// <summary>
+        /// メールリンク認証
+        /// </summary>
+        void SignInWithEmailLink();
+
+        /// <summary>
+        /// Facebook認証
+        /// </summary>
+        void SignInWithFacebook();
+
+        /// <summary>
+        /// Twitter認証
+        /// </summary>
+        void SignInWithTwitter();
+
+        /// <summary>
+        /// Google認証
+        /// </summary>
+        void SignInWithGoogle();
+
+        /// <summary>
+        /// Microsoft認証
+        /// </summary>
+        void SignInWithMicrosoft();
+
+        /// <summary>
+        /// Apple認証
+        /// </summary>
+        void SignInWithApple();
+
+        /// <summary>
+        /// LINE認証
+        /// </summary>
+        void SignInWithLine();
+
+        /// <summary>
+        /// SMS認証
+        /// </summary>
+        void SignInWithSMS();
     }
 }
