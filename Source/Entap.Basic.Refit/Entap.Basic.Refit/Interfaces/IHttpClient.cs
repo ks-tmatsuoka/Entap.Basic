97e7cb7b78a8d2476125be9b805808d22d4e8db6
Iga <iga@entap.co.jp>
2021-03-11 15:02:42 +0900

    [add]Entap.Basic.Refit : IHttpClient

---
+++ Source/Entap.Basic.Refit/Entap.Basic.Refit/Interfaces/IHttpClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Entap.Basic.Refit
+{
+    /// <summary>
+    /// HttpClientインターフェース
+    /// RestServiceからHttpClientにアクセス可能にする
+    /// </summary>
+    public interface IHttpClient
+    {
+        HttpClient Client { get; }
+    }
+}
