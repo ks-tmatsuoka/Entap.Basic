dbcb7eac50e74b493e4b5ad2457553724c3e9a57
Iga <iga@entap.co.jp>
2021-03-11 17:43:12 +0900

    [update]EntapBasic.Api : エラー判定用モデル定義

---
+++ Source/Entap.Basic/Entap.Basic.Api/Models/ErrorResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Entap.Basic.Api
+{
+    public class ErrorResponse
+    {
+        public ErrorResponse()
+        {
+        }
+
+        /// <summary>
+        /// メッセージ
+        /// </summary>
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        /// <summary>
+        /// エラー情報
+        /// Errors内のプロパティが可変なので、JObjectとして定義
+        /// </summary>
+        [JsonProperty("errors")]
+        public JObject Errors { get; set; }
+    }
+}
---
+++ Source/Entap.Basic/Entap.Basic.Api/Models/ExHttpStatusCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+
+namespace Entap.Basic.Api
+{
+    /// <summary>
+    /// System.Net.HttpStatusCodeに定義のないコードを定義する
+    /// https://docs.microsoft.com/en-us/dotnet/api/system.net.httpstatuscode?view=netframework-2.0
+    /// </summary>
+    public struct ExHttpStatusCode
+    {
+        public const HttpStatusCode UnprocessableEntity = (HttpStatusCode)422;
+    }
+}
