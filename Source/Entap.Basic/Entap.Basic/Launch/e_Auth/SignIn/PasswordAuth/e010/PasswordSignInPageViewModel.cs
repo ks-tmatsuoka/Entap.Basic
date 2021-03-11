01d2e2b80158e85941eb5f664bcc0ecac2533488
Iga <iga@entap.co.jp>
2021-03-11 09:38:06 +0900

    [update]Entap.Basic : PasswordSignInPage : コントロール配置、ViewModel実装

--- Source/Entap.Basic/Entap.Basic/Launch/e_Auth/SignIn/PasswordAuth/e010/PasswordSignInPageViewModel.cs
+++ Source/Entap.Basic/Entap.Basic/Launch/e_Auth/SignIn/PasswordAuth/e010/PasswordSignInPageViewModel.cs
@@ -1,13 +1,67 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Entap.Basic.Forms;
 using Xamarin.Forms;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Entap.Basic.Launch.Auth
 {
     public class PasswordSignInPageViewModel : PageViewModelBase
     {
+        readonly IPasswordSignInUseCase _passwordSignInUseCase;
         public PasswordSignInPageViewModel()
         {
+            _passwordSignInUseCase = Startup.ServiceProvider.GetService<IPasswordSignInUseCase>();
+            SetPageLifeCycle(_passwordSignInUseCase);
+
+            SignInCommand = new Command(
+                () => _passwordSignInUseCase.SignIn(MailAddress, Password),
+                () =>
+                    !string.IsNullOrEmpty(MailAddress) &&
+                    !IsMailAddressError &&
+                    !string.IsNullOrEmpty(Password));
         }
+
+        public string MailAddress
+        {
+            get => _mailAddress;
+            set
+            {
+                if (SetProperty(ref _mailAddress, value))
+                {
+                    OnPropertyChanged(nameof(MailAddressErrorMessage));
+                    OnPropertyChanged(nameof(IsMailAddressError));
+                    SignInCommand?.ChangeCanExecute();
+                }
+            }
+        }
+        string _mailAddress;
+
+        public string MailAddressErrorMessage => _passwordSignInUseCase.ValidateMailAddress(MailAddress);
+
+        public bool IsMailAddressError => !string.IsNullOrEmpty(MailAddressErrorMessage);
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (SetProperty(ref _password, value))
+                {
+                    OnPropertyChanged(nameof(PasswordErrorMessage));
+                    OnPropertyChanged(nameof(IsPasswordError));
+                    SignInCommand?.ChangeCanExecute();
+                }
+            }
+        }
+        string _password;
+
+        public string PasswordErrorMessage => _passwordSignInUseCase.ValidatePassword(Password);
+
+        public bool IsPasswordError => !string.IsNullOrEmpty(PasswordErrorMessage);
+
+        public Command SignInCommand { get; set; }
+
+        public Command ResetPasswordCommand => new Command(() => _passwordSignInUseCase.ResetPassword());
     }
 }
