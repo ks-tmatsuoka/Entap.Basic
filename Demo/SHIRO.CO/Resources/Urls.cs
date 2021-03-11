2d9e3b8b1ebd83a1c52da20b1715065bd9870ac7
Iga <iga@entap.co.jp>
2021-03-11 12:17:06 +0900

    [add]Demo : Urls : 開発サーバのURLを定義

---
+++ Demo/SHIRO.CO/Resources/Urls.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SHIRO.CO
+{
+    public class Urls
+    {
+        public Urls()
+        {
+        }
+
+        public static readonly string AppHost = "https://grasshopper.entap.dev";
+        public static readonly string AppApi = $"{AppHost}/api";
+    }
+}
