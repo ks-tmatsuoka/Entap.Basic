5eb3ab6a4746b17e3daa01e56898c984f4944115
Iga <iga@entap.co.jp>
2021-03-11 15:14:03 +0900

    [add]Entap.Basic.Refit : RefitSettingsService : SnakeCase, CamelCase用のシリアライザ取得処理定義

---
+++ Source/Entap.Basic.Refit/Entap.Basic.Refit/Modules/RefitSettingsService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Refit;
+
+namespace Entap.Basic.Refit
+{
+    public static class RefitSettingsService
+    {
+        #region ContentSerializer
+        /// <summary>
+        /// SnakeCase用のシリアライザを取得する
+        /// </summary>
+        public static NewtonsoftJsonContentSerializer SnakeCaseSerializer
+            => new NewtonsoftJsonContentSerializer(
+                new JsonSerializerSettings
+                {
+                    ContractResolver = new DefaultContractResolver
+                    {
+                        NamingStrategy = new SnakeCaseNamingStrategy()
+                    }
+                });
+
+        /// <summary>
+        /// CamelCase用のシリアライザを取得する
+        /// </summary>
+        public static NewtonsoftJsonContentSerializer CamelCaseSerializer
+            => new NewtonsoftJsonContentSerializer(
+                new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                });
+        #endregion
+    }
+}
