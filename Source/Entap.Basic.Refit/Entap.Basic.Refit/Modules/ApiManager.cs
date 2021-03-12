75744a984211ffd567fb7be8c6ace2cc4107ac02
Iga <iga@entap.co.jp>
2021-03-12 12:43:31 +0900

    [add]Entap.Basic.Refit : ApiManager, ApiService追加

---
+++ Source/Entap.Basic.Refit/Entap.Basic.Refit/Modules/ApiManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Entap.Basic.Core;
+using Refit;
+
+namespace Entap.Basic.Refit
+{
+    /// <summary>
+    /// APIのRestServiceを管理する
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ApiManager<T> : ApiService where T : IHttpClient
+    {
+#nullable enable
+        public ApiManager(string hostUrl, RefitSettings? refitSettings = null)
+#nullable disable
+        {
+            Api = GetRestService<T>(hostUrl, refitSettings);
+        }
+
+        /// <summary>
+        /// RestService
+        /// </summary>
+        public T Api { get; }
+
+        /// <summary>
+        /// RestServiceを取得する
+        /// </summary>
+        /// <typeparam name="T">RestService</typeparam>
+        /// <param name="hostUrl">ホストUrl</param>
+        /// <param name="refitSettings">RefitSettings</param>
+        /// <returns></returns>
+#nullable enable
+        public virtual T GetRestService<T>(string hostUrl, RefitSettings? refitSettings = null) where T : IHttpClient
+#nullable disable
+        {
+            return RestService.For<T>(hostUrl, refitSettings);
+        }
+
+        /// <summary>
+        /// 認証情報を設定する
+        /// </summary>
+        /// <param name="httpClient">RestService</param>
+        /// <param name="type">認証方法</param>
+        /// <param name="token">トークン</param>
+        public virtual void SetAuthorization(IHttpClient restService, string type, string token)
+        {
+            restService.Client.SetAuthorization(type, token);
+        }
+    }
+}
---
+++ Source/Entap.Basic.Refit/Entap.Basic.Refit/Modules/ApiService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using Refit;
+
+namespace Entap.Basic.Refit
+{
+    /// <summary>
+    /// APIの実行処理
+    /// </summary>
+    public class ApiService
+    {
+        public ApiService()
+        {
+        }
+
+        /// <summary>
+        /// APIをコールする（APIレスポンスあり）
+        /// </summary>
+        /// <typeparam name="T">レスポンスの型</typeparam>
+        /// <param name="funcTask">API処理</param>
+        /// <param name="ignoreError">エラーを無視するか</param>
+#nullable enable
+        public virtual async Task<ApiResponse<T>?> CallAsync<T>(Func<Task<ApiResponse<T>>> funcTask, bool ignoreError = false)
+#nullable disable
+        {
+            try
+            {
+                var result = await funcTask().ConfigureAwait(false);
+                if (result.IsSuccessStatusCode) return result;
+                if (ignoreError) return result;
+
+                HandleApiError(result.Error, ignoreError);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex, ignoreError);
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// APIをコールする（APIレスポンスなし）
+        /// </summary>
+        /// <param name="funcTask">API処理</param>
+        /// <param name="ignoreError">エラーを無視するか</param>
+#nullable enable
+        public virtual Task<ApiResponse<Task>?> CallAsync(Func<ApiResponse<Task>> funcTask, bool ignoreError = false)
+#nullable disable
+        {
+            return CallAsync(funcTask, ignoreError);
+        }
+
+        /// <summary>
+        /// ApiExceptionハンドリング処理
+        /// </summary>
+        /// <param name="apiException">ApiException</param>
+        public virtual void HandleApiError(ApiException apiException, bool ignoreError)
+        {
+        }
+
+        /// <summary>
+        /// 例外ハンドリング処理
+        /// </summary>
+        /// <param name="exception">Exception</param>
+        public virtual void HandleException(Exception exception, bool ignoreError)
+        {
+        }
+    }
+}
