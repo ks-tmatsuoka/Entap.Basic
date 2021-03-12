6d08df7b5a9c1f50e52205edddc970311b25fa49
Iga <iga@entap.co.jp>
2021-03-12 10:05:31 +0900

    [add]Entap.Basic.Core : HttpClientExtensions追加

---
+++ Source/Entap.Basic.Core/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Entap.Basic.Core
+{
+    public static class HttpClientExtensions
+    {
+        /// <summary>
+        /// JsonAcceptヘッダを設定する
+        /// </summary>
+        /// <param name="httpClient">HttpClient</param>
+        public static void SetJsonAcceptHeader(this HttpClient httpClient)
+        {
+            SetAcceptHeader(httpClient, "application/json");
+        }
+
+        /// <summary>
+        /// Acceptヘッダを設定する
+        /// </summary>
+        /// <param name="httpClient">HttpClient</param>
+        /// <param name="mediaType"></param>
+        public static void SetAcceptHeader(this HttpClient httpClient, string mediaType)
+        {
+            ;
+            httpClient.DefaultRequestHeaders.Add("Accept", System.Net.Mime.MediaTypeNames.Application.Json);
+        }
+
+        /// <summary>
+        /// 認証情報を設定する
+        /// </summary>
+        /// <param name="httpClient">HttpClient</param>
+        /// <param name="type">認証方法</param>
+        /// <param name="token">トークン</param>
+        public static void SetAuthorization(this HttpClient httpClient, string type, string token)
+        {
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(type, token);
+        }
+    }
+}
