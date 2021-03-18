9c22b061eaa008aa37b19a64ac8f46770b1bc38e
Iga <iga@entap.co.jp>
2021-03-18 15:36:30 +0900

    [update]Entap.Basic : BasicPasswordSignInUseCase

--- Source/Entap.Basic/Entap.Basic/Launch/e_Auth/SignIn/PasswordAuth/e010/BasicPasswordSingnInUseCase.cs
+++ Source/Entap.Basic/Entap.Basic/Launch/e_Auth/SignIn/PasswordAuth/e010/BasicPasswordSingnInUseCase.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using Entap.Basic.Auth.Abstractions;
+using Entap.Basic.Core;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Entap.Basic.Launch.Auth
 {
@@ -9,8 +12,12 @@ namespace Entap.Basic.Launch.Auth
         public static readonly string RegexMailAddress = @"^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
         public static readonly string RegexPassword = @"^[!-~]*$";
 
+        readonly IPageNavigator _pageNavigator;
+        readonly IPasswordAuthService _passwordAuthService;
         public BasicPasswordSingnInUseCase()
         {
+            _pageNavigator = Startup.ServiceProvider.GetService<IPageNavigator>();
+            _passwordAuthService = Startup.ServiceProvider.GetService<IPasswordAuthService>();
         }
 
         public string ValidateMailAddress(string mailAddress)
@@ -31,7 +38,12 @@ namespace Entap.Basic.Launch.Auth
 
         public void SignIn(string mailAddress, string password)
         {
-            throw new NotImplementedException();
+            ProcessManager.Current.Invoke(async () =>
+            {
+                var token = await _passwordAuthService.SignInAsync(mailAddress, password);
+                if (string.IsNullOrEmpty(token)) return;
+                _pageNavigator.SetHomePage();
+            });
         }
 
         public void ResetPassword()
