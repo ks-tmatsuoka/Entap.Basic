b25fa7c66f6e562cb5a1fabb96383eb6ed545132
Iga <iga@entap.co.jp>
2021-03-23 08:34:58 +0900

    [add]Entap.Basic.Forms : Entap.Basic.Forms.Android : Platform : 初期化処理実装

---
+++ Source/Entap.Basic.Forms/Entap.Basic.Forms.Android/Platform.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+
+namespace Entap.Basic.Forms.Android
+{
+    [Preserve(AllMembers = true)]
+    public　static class Platform
+    {
+        public static void Init(Activity activity)
+        {
+            Entap.Basic.Core.Android.Platform.Init(activity);
+        }
+    }
+}
