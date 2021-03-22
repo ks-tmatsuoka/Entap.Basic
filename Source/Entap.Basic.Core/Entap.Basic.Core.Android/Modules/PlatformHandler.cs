4a6e56b675c0771ec7ca7e8ea40f2f00026b79b2
Iga <iga@entap.co.jp>
2021-03-22 15:52:22 +0900

    [add]Entap.Basic.Core : Entap.Basic.Android : 初期化処理、PlatformHandler定義
    PlatformHandler : Android 8.0、8.1でAutofill使用時のクラッシュ回避処理実装

---
+++ Source/Entap.Basic.Core/Entap.Basic.Core.Android/Modules/PlatformHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Views;
+
+namespace Entap.Basic.Core.Android
+{
+    /// <summary>
+    /// プラットフォーム固有処理ハンドラー
+    /// </summary>
+    [Preserve(AllMembers = true)]
+    public class PlatformHandler
+    {
+        public PlatformHandler()
+        {
+        }
+
+        /// <summary>
+        /// プラットフォーム固有処理を実行する
+        /// </summary>
+        /// <param name="activity">Activity</param>
+        public static void Handle(Activity activity)
+        {
+            HandleAutofill(activity);
+        }
+
+        /// <summary>
+        /// 自動入力の制御
+        /// 自動入力により、Android 8.0、8.1でアプリがクラッシュ
+        /// https://developer.android.com/guide/topics/text/autofill-optimize#autofill_causes_apps_to_crash_on_android_80_81
+        /// </summary>
+        public static void HandleAutofill(Activity activity)
+        {
+            if (Build.VERSION.SdkInt == BuildVersionCodes.O ||
+                Build.VERSION.SdkInt == BuildVersionCodes.OMr1)
+            {
+                activity.Window.DecorView.ImportantForAutofill = ImportantForAutofill.NoExcludeDescendants;
+            }
+        }
+    }
+}
---
+++ Source/Entap.Basic.Core/Entap.Basic.Core.Android/Platform.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+
+namespace Entap.Basic.Core.Android
+{
+    [Preserve(AllMembers = true)]
+    public class Platform
+    {
+        public static void Init(Activity activity)
+        {
+            PlatformHandler.Handle(activity);
+        }
+    }
+}
