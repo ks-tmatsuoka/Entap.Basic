b9774c2e080b0c10e9ac28b4e24af53ef5694d3d
Iga <iga@entap.co.jp>
2021-03-24 09:55:27 +0900

    [add]Sample : Android : StarterActivityのサンプル処理追加

---
+++ Sample/Sample.Android/Modules/StarterActivityService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.Content;
+using Xamarin.Essentials;
+using Entap.Basic.Core.Android;
+
+namespace Sample.Droid
+{
+    public class StarterActivityService
+    {
+        public StarterActivityService()
+        {
+        }
+
+        public async Task<string> PickPhotoAsync()
+        {
+            var intent = new Intent(Intent.ActionGetContent);
+            intent.SetType("image/*");
+            var pickerIntent = Intent.CreateChooser(intent, "");
+            var activity = Xamarin.Essentials.Platform.CurrentActivity;
+
+            try
+            {
+                var result = await StarterActivity.StartAsync(activity, pickerIntent, 1000);
+                return result.Data.ToString();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
