57055fb3198c48c8028c8ccc974f96c9fae1ead3
Iga <iga@entap.co.jp>
2021-03-24 10:12:00 +0900

    [update]Demo : Entap.Basic.Core.Androidの参照追加

--- Demo/SHIRO.CO.Android/MainActivity.cs
+++ Demo/SHIRO.CO.Android/MainActivity.cs
@@ -21,6 +21,7 @@ namespace SHIRO.CO.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Entap.Basic.Android.Platform.Init(this);
             Entap.Basic.Firebase.Auth.Android.Platform.Init(Application.Context, this, savedInstanceState);
             LoadApplication(new App());
         }
