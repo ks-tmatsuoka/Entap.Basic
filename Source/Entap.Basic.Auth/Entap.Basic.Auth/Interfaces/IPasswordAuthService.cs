6408cd9b0d97640ee31ab3babb14c150c59e471f
Iga <iga@entap.co.jp>
2021-04-05 10:29:22 +0900

    [update]Entap.Basic.Auth : IPasswordAuthServiceにパスワード失念時の、メール送信認証処理定義

--- Source/Entap.Basic.Auth/Entap.Basic.Auth/Interfaces/IPasswordAuthService.cs
+++ Source/Entap.Basic.Auth/Entap.Basic.Auth/Interfaces/IPasswordAuthService.cs
@@ -9,6 +9,12 @@ namespace Entap.Basic.Auth.Abstractions
         Task<string?> SignUpAsync(string email, string password);
         Task<string?> SignInAsync(string email, string password);
         #nullable disable
+
+        Task SendPasswordResetEmailAsync(string email);
+
         void SignOut();
+
+        Task<string> VerifyPasswordResetCodeAsync(string actionCode);
+        Task ConfirmPasswordResetAsync(string actionCode, string newPassword);
     }
 }
