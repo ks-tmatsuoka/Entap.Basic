06425057ca7e9296171834cdf06c34bfdbdb52ba
Iga <iga@entap.co.jp>
2021-04-05 10:44:49 +0900

    [add]Entap.Basic.Firebase.Auth : ErrorCode定義

---
+++ Source/Entap.Basic.Firebase.Auth/Entap.Basic.Firebase.Auth/Modules/ErrorCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Entap.Basic.Firebase.Auth
+{
+    /// <summary>
+    /// Firebase.AuthのErrorCodeを定義
+    /// Plugin.FirebaseAuthでは、ErrorTypeに集約されたエラーをエラーコードによって判別可能とする
+    /// https://github.com/f-miyu/Plugin.FirebaseAuth/blob/master/Plugin.FirebaseAuth/iOS/ExceptionMapper.cs
+    /// https://github.com/f-miyu/Plugin.FirebaseAuth/blob/master/Plugin.FirebaseAuth/Android/ExceptionMapper.cs
+    /// </summary>
+    public struct ErrorCode
+    {
+        public const string UserDisabled = "ERROR_USER_DISABLED";
+    }
+}
