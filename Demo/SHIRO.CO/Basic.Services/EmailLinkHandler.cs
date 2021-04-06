291a425af905a5a14d21c32fe4a74c8c599e7a47
Iga <iga@entap.co.jp>
2021-04-06 09:22:45 +0900

    [add]Demo : EmailLinkHandler

---
+++ Demo/SHIRO.CO/Basic.Services/EmailLinkHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Entap.Basic;
+using Entap.Basic.Firebase.Auth.EmailLink;
+using Entap.Basic.Forms;
+using Entap.Basic.Launch.Auth;
+
+namespace SHIRO.CO
+{
+    public class EmailLinkHandler : Entap.Basic.Firebase.Auth.EmailLink.EmailLinkHandler
+    {
+        new public static EmailLinkHandler Current => LazyInitializer.Value;
+        static readonly Lazy<EmailLinkHandler> LazyInitializer = new Lazy<EmailLinkHandler>(() => new EmailLinkHandler());
+
+        public EmailLinkHandler()
+        {
+        }
+
+        public override async void OnResetPassword(EmailActionParameter parameter)
+        {
+            base.OnResetPassword(parameter);
+            var email = await BasicStartup.AuthService.VerifyPasswordResetCodeAsync(parameter.OobCode);
+            if (string.IsNullOrEmpty(email)) return;
+
+            await PageManager.Navigation.PushNavigationModalAsync<ResetPasswordPage>(new ResetPasswordPageViewModel(parameter.OobCode));
+        }
+    }
+}
