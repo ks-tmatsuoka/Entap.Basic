36a97e28a0a28e2f7b8e085b94560aa94d4e5a05
Iga <iga@entap.co.jp>
2021-04-06 16:09:39 +0900

    [update]Entap.Basic.Firebase.Auth : SnsAuthService : Plugin.FirebaseAuthのサインインのラップ処理定義

--- Source/Entap.Basic.Firebase.Auth/Entap.Basic.Firebase.Auth/Modules/SnsAuthService.cs
+++ Source/Entap.Basic.Firebase.Auth/Entap.Basic.Firebase.Auth/Modules/SnsAuthService.cs
@@ -1,10 +1,19 @@
 ﻿using System;
-namespace Entap.Basic.Firebase.Auth.Modules
+using System.Threading.Tasks;
+using Plugin.FirebaseAuth;
+
+namespace Entap.Basic.Firebase.Auth
 {
     public class SnsAuthService
     {
         public SnsAuthService()
         {
         }
+
+        protected Task SignInWithProviderAsync(IFederatedAuthProvider provider)
+            => CrossFirebaseAuth.Current.Instance.SignInWithProviderAsync(provider);
+
+        protected Task SignInWithCredentialAsync(IAuthCredential credential)
+            => CrossFirebaseAuth.Current.Instance.SignInWithCredentialAsync(credential);
     }
 }
