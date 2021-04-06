e2bc9cb0766be65a80c81b109cdf92f86652e772
Iga <iga@entap.co.jp>
2021-04-06 09:30:28 +0900

    [update]Demo : Android : DynamicLinks実装

--- Demo/SHIRO.CO.Android/MainActivity.cs
+++ Demo/SHIRO.CO.Android/MainActivity.cs
@@ -6,11 +6,22 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Gms.Tasks;
+using Firebase.DynamicLinks;
 
 namespace SHIRO.CO.Droid
 {
     [Activity(Label = "SHIRO.CO", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    [IntentFilter(new[] { "android.intent.action.VIEW" },
+        Categories = new[]
+        {
+            "android.intent.category.DEFAULT",
+            "android.intent.category.BROWSABLE"
+        },
+        DataScheme = "http", DataHost = "entapshiro.page.link"
+        )
+    ]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity, IOnSuccessListener
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -23,6 +34,9 @@ namespace SHIRO.CO.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             Entap.Basic.Android.Platform.Init(this);
             Entap.Basic.Firebase.Auth.Android.Platform.Init(Application.Context, this, savedInstanceState);
+
+            Firebase.DynamicLinks.FirebaseDynamicLinks.Instance.GetDynamicLink(this.Intent)
+                .AddOnSuccessListener(this, this);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
@@ -30,6 +44,21 @@ namespace SHIRO.CO.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void OnSuccess(Java.Lang.Object result)
+        {
+            if (result is null) return;
+            if (!(result is PendingDynamicLinkData linkData)) return;
+
+            try
+            {
+                EmailLinkHandler.Current.HandleEmailAction(linkData.Link.ToString()).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
         }
     }
 }
