4c0cebce0ad050fd50601b9f7fad4211c102c060
Iga <iga@entap.co.jp>
2021-04-06 16:15:24 +0900

    [add]Entap.Basic.Firebase.Auth : TwitterAuthService定義

---
+++ Source/Entap.Basic.Firebase.Auth/Entap.Basic.Firebase.Auth/Modules/TwitterAuthService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Entap.Basic.Auth;
+using Plugin.FirebaseAuth;
+
+namespace Entap.Basic.Firebase.Auth
+{
+    public class TwitterAuthService : SnsAuthService, ISnsAuthService
+    {
+        const string ProviderId = "twitter.com";
+
+        public TwitterAuthService()
+        {
+        }
+
+        public Task SignInAsync()
+        {
+            var provider = new OAuthProvider(ProviderId);
+            return SignInWithProviderAsync(provider);
+        }
+    }
+}
