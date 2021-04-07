6ccbbee47e26436840de90974364a1b3d4117a04
Iga <iga@entap.co.jp>
2021-04-07 09:59:12 +0900

    [add]Entap.Basic.Firebase.Auth : IPasswordAuthErrorCallback : パスワード認証エラー時のCallback Interface定義

---
+++ Source/Entap.Basic.Firebase.Auth/Entap.Basic.Firebase.Auth/Interfaces/IPasswordAuthErrorCallback.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Entap.Basic.Firebase.Auth
+{
+    public interface IPasswordAuthErrorCallback : IAuthErrorCallback
+    {
+        Task HandleSignUpErrorAsync(Exception exception);
+        Task HandleSendPasswordResetEmailErrorAsync(Exception exception);
+        Task HandleVerifyPasswordResetCodeErrorAsync(Exception exception);
+        Task HandleConfirmPasswordResetErrorAsync(Exception exception);
+    }
+}
