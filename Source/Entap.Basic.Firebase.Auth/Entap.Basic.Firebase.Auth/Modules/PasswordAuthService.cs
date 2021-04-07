27de3586ce33040a8c512d204996b8bc1f1951a6
Iga <iga@entap.co.jp>
2021-04-07 10:22:07 +0900

    [update]Entap.Basic.Firebase.Auth : PasswordAuthService : エラー時のコールバック実行

--- Source/Entap.Basic.Firebase.Auth/Entap.Basic.Firebase.Auth/Modules/PasswordAuthService.cs
+++ Source/Entap.Basic.Firebase.Auth/Entap.Basic.Firebase.Auth/Modules/PasswordAuthService.cs
@@ -7,8 +7,10 @@ namespace Entap.Basic.Firebase.Auth
 {
     public class PasswordAuthService : IPasswordAuthService
     {
-        public PasswordAuthService()
+        readonly IPasswordAuthErrorCallback _errorCallback;
+        public PasswordAuthService(IPasswordAuthErrorCallback errorCallback)
         {
+            _errorCallback = errorCallback;
         }
 
 
@@ -23,8 +25,16 @@ namespace Entap.Basic.Firebase.Auth
         public async Task<string?> SignUpAsync(string email, string password)
 #nullable disable
         {
-            var result = await CrossFirebaseAuth.Current.Instance.CreateUserWithEmailAndPasswordAsync(email, password);
-            return await result?.User?.GetIdTokenAsync(false);
+            try
+            {
+                var result = await CrossFirebaseAuth.Current.Instance.CreateUserWithEmailAndPasswordAsync(email, password);
+                return await result?.User?.GetIdTokenAsync(false);
+            }
+            catch (Exception ex)
+            {
+                await _errorCallback.HandleSignUpErrorAsync(ex);
+                throw ex;
+            }
         }
 
         /// <summary>
@@ -37,8 +47,16 @@ namespace Entap.Basic.Firebase.Auth
         public async Task<string?> SignInAsync(string email, string password)
 #nullable disable
         {
-            var result = await CrossFirebaseAuth.Current.Instance.SignInWithEmailAndPasswordAsync(email, password);
-            return await result?.User?.GetIdTokenAsync(false);
+            try
+            {
+                var result = await CrossFirebaseAuth.Current.Instance.SignInWithEmailAndPasswordAsync(email, password);
+                return await result?.User?.GetIdTokenAsync(false);
+            }
+            catch (Exception ex)
+            {
+                await _errorCallback.HandleSignInErrorAsync(ex);
+                throw ex;
+            }
         }
 
         /// <summary>
@@ -55,30 +73,60 @@ namespace Entap.Basic.Firebase.Auth
         /// <param name="email">メールアドレス</param>
         /// https://firebase.google.com/docs/reference/js/firebase.auth.Auth?hl=ja#sendpasswordresetemail
         public Task SendPasswordResetEmailAsync(string email)
-            => CrossFirebaseAuth.Current.Instance.SendPasswordResetEmailAsync(email);
+            => SendPasswordResetEmailAsync(email, null);
 
         /// <summary>
         /// パスワードリセットメール送信
         /// </summary>
         /// <param name="BasicSendPasswordResetEmailParameter">SendPasswordResetEmailParameter</param>
         /// https://firebase.google.com/docs/reference/js/firebase.auth.Auth?hl=ja#sendpasswordresetemail
-        public Task SendPasswordResetEmailAsync(string email, ActionCodeSettings settings)
-            => CrossFirebaseAuth.Current.Instance.SendPasswordResetEmailAsync(email, settings);
+        public async Task SendPasswordResetEmailAsync(string email, ActionCodeSettings settings)
+        {
+            try
+            { 
+                await CrossFirebaseAuth.Current.Instance.SendPasswordResetEmailAsync(email, settings);
+            }
+            catch (Exception ex)
+            {
+                await _errorCallback.HandleSendPasswordResetEmailErrorAsync(ex);
+                throw ex;
+            }
+        }
 
         /// <summary>
         /// パスワードリセット時のアクションコード検証
         /// </summary>
         /// <param name="actionCode">パスワードリセットメールのURLに保有するアクションコード</param>
         /// <returns>メールアドレス</returns>
-        public Task<string> VerifyPasswordResetCodeAsync(string actionCode)
-            => CrossFirebaseAuth.Current.Instance.VerifyPasswordResetCodeAsync(actionCode);
+        public async Task<string> VerifyPasswordResetCodeAsync(string actionCode)
+        {
+            try
+            {
+                return await CrossFirebaseAuth.Current.Instance.VerifyPasswordResetCodeAsync(actionCode);
+            }
+            catch (Exception ex)
+            {
+                await _errorCallback.HandleVerifyPasswordResetCodeErrorAsync(ex);
+                throw ex;
+            }
+        }
 
         /// <summary>
         /// パスワードリセット処理
         /// </summary>
         /// <param name="actionCode">パスワードリセットメールのURLに保有するアクションコード</param>
         /// <param name="newPassword">パスワード</param>
-        public Task ConfirmPasswordResetAsync(string actionCode, string newPassword)
-            => CrossFirebaseAuth.Current.Instance.ConfirmPasswordResetAsync(actionCode, newPassword);
+        public async Task ConfirmPasswordResetAsync(string actionCode, string newPassword)
+        {
+            try
+            {
+                await CrossFirebaseAuth.Current.Instance.ConfirmPasswordResetAsync(actionCode, newPassword);
+            }
+            catch (Exception ex)
+            {
+                await _errorCallback.HandleConfirmPasswordResetErrorAsync(ex);
+                throw ex;
+            }
+        }
     }
 }
