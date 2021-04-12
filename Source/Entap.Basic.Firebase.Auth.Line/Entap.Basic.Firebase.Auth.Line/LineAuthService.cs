c6ee88ff03a5e439437cd2f7b47257b4370e3656
Iga <iga@entap.co.jp>
2021-04-13 08:36:51 +0900

    [add]Entap.Basic.Firebase.Auth.Line : LineAuthService

---
+++ Source/Entap.Basic.Firebase.Auth.Line/Entap.Basic.Firebase.Auth.Line/LineAuthService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Entap.Basic.Auth;
+
+namespace Entap.Basic.Firebase.Auth.Line
+{
+    public class LineAuthService : SnsAuthService, ISnsAuthService
+    {
+        public LineAuthService()
+        {
+        }
+
+        public Task SignInAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
