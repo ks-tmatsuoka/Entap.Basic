a8ea0920eaf183318c46cabc8fc0f15b9210aec3
Iga <iga@entap.co.jp>
2021-04-13 16:27:51 +0900

    [add]Entap.Basic.Auth : OAuth2.AccessTokenResponse

---
+++ Source/Entap.Basic.Auth/Entap.Basic.Auth/OAuth2/AccessTokenResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Entap.Basic.Auth.OAuth2
+{
+    /// <summary>
+    /// アクセストークンレスポンス
+    /// https://openid-foundation-japan.github.io/rfc6749.ja.html#implicit-authz-resp
+    /// </summary>
+    public class AccessTokenResponse
+    {
+        public AccessTokenResponse()
+        {
+        }
+
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonProperty("ExpiresIn")]
+        public int ExpiresIn { get; set; }
+
+        [JsonProperty("scope")]
+        public int Scope { get; set; }
+
+        [JsonProperty("state")]
+        public int State { get; set; }
+    }
+}
