27b5ddd5823e84f7d722a4b8d59b3205bc436435
Iga <iga@entap.co.jp>
2021-04-13 16:29:34 +0900

    [add]Entap.Basic.Auth.Line : LineAccessTokenRequest, LineAccessTokenResponse

---
+++ Source/Entap.Basic.Auth.Line/Entap.Basic.Auth.Line/Models/LineAccessTokenResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Entap.Basic.Auth.OAuth2;
+using Newtonsoft.Json;
+
+namespace Entap.Basic.Auth.Line
+{
+    /// <summary>
+    /// アクセストークンレスポンス
+    /// https://developers.line.biz/ja/reference/line-login/#issue-token-response
+    /// </summary>
+    public class LineAccessTokenResponse : AccessTokenResponse
+    {
+        public LineAccessTokenResponse()
+        {
+        }
+
+        [JsonProperty("id_token")]
+        public string IdToken { get; set; }
+
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
+    }
+}
