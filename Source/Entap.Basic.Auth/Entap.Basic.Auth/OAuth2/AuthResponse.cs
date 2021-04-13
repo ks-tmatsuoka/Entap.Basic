6bb1f80685b3fcc09e94e8c3b0807b00a6cda63c
Iga <iga@entap.co.jp>
2021-04-13 10:00:13 +0900

    [add]Entap.Basic.Auth : OAuth2.AuthResponse

---
+++ Source/Entap.Basic.Auth/Entap.Basic.Auth/OAuth2/AuthResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Entap.Basic.Auth.OAuth2
+{
+    /// <summary>
+    /// 認可レスポンス
+    /// https://openid-foundation-japan.github.io/rfc6749.ja.html#code-authz-resp
+    /// </summary>
+    public class AuthResponse
+    {
+        /// <summary>
+        /// 認可コード
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// リクエストとコールバックの間で状態を維持するために使用するランダムな値
+        /// </summary>
+        public string State { get; set; }
+    }
+}
