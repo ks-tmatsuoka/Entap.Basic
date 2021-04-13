ca0a2ef0120a16facff4453f35e56fe10dcdd772
Iga <iga@entap.co.jp>
2021-04-13 15:56:24 +0900

    [add]Entap.Basic.Auth : OAuth2.AccessTokenRequest

---
+++ Source/Entap.Basic.Auth/Entap.Basic.Auth/OAuth2/AccessTokenRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Entap.Basic.Auth.OAuth2
+{
+    /// <summary>
+    /// アクセストークンリクエスト
+    /// https://openid-foundation-japan.github.io/rfc6749.ja.html#token-req
+    /// </summary>
+    public class AccessTokenRequest
+    {
+        public AccessTokenRequest()
+        {
+        }
+
+        [JsonProperty("grant_type")]
+        public string GrantType { get; set; }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("redirect_uri")]
+        public string RedirectUri { get; set; }
+
+        [JsonProperty("client_id")]
+        public string ClientId { get; set; }
+    }
+}
