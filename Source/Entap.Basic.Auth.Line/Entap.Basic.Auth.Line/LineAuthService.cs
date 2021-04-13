2d80989720b075e74bf402886db5935384967e38
Iga <iga@entap.co.jp>
2021-04-13 17:31:45 +0900

    [update]Entap.Basic.Auth.Line : LineAuthService : アクセストークン取得処理実装

--- Source/Entap.Basic.Auth.Line/Entap.Basic.Auth.Line/LineAuthService.cs
+++ Source/Entap.Basic.Auth.Line/Entap.Basic.Auth.Line/LineAuthService.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Xamarin.Essentials;
@@ -9,7 +12,8 @@ namespace Entap.Basic.Auth.Line
 {
     public class LineAuthService
     {
-        const string AuthBaseUri = "https://access.line.me/oauth2/v2.1/authorize";
+        readonly string AuthBaseUri = "https://access.line.me/oauth2/v2.1/authorize";
+        readonly string TokenBaseUri = "https://api.line.me/oauth2/v2.1/token";
         public LineAuthService()
         {
         }
@@ -32,5 +36,29 @@ namespace Entap.Basic.Auth.Line
             return UriService.GetQueryObject<LineAuthResponse>(parameters);
         }
         #endregion
+
+#nullable enable
+        /// <summary>
+        /// アクセストークン取得処理
+        /// https://developers.line.biz/ja/reference/line-login/#issue-access-token
+        /// </summary>
+        /// <param name="request">LineAccessTokenRequest</param>
+        /// <returns>HttpStatusCode, LineAccessTokenResponse</returns>
+        public async Task<(HttpStatusCode, LineAccessTokenResponse?)> GetAccessTokenAsync(LineAccessTokenRequest request)
+#nullable disable
+        {
+            var message = new HttpRequestMessage(HttpMethod.Post, TokenBaseUri);
+            var json = JsonConvert.SerializeObject(request);
+            var dictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+            message.Content = new FormUrlEncodedContent(dictionary);
+
+            using var client = new HttpClient();
+            var response = await client.SendAsync(message);
+            if (!response.IsSuccessStatusCode)
+                return (response.StatusCode, null);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            return (response.StatusCode, JsonConvert.DeserializeObject<LineAccessTokenResponse>(responseString));
+        }
     }
 }
