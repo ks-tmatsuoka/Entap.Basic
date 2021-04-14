f0dc023b23ad71550a36f26ee2765c5a90b31828
Iga <iga@entap.co.jp>
2021-04-14 11:12:06 +0900

    [add]Entap.Basic.Firebase.Auth : BasicAuthStartUp（DIコンテナラップ処理）定義
    PackageReference : Microsoft.Extensions.DependencyInjection追加
    ProjectReference : Entap.Basic.Api追加

---
+++ Source/Entap.Basic.Firebase.Auth/Entap.Basic.Firebase.Auth/BasicAuthStartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Entap.Basic.Api;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Entap.Basic.Firebase.Auth
+{
+    public static class BasicAuthStartUp
+    {
+        private static readonly ServiceCollection _serviceCollection;
+        static BasicAuthStartUp()
+        {
+            _serviceCollection = new ServiceCollection();
+        }
+
+        static IServiceProvider ServiceProvider => _serviceCollection.BuildServiceProvider();
+
+        #region AuthApi
+        /// <summary>
+        /// AuthApiの登録
+        /// </summary>
+        /// <typeparam name="TImplementation">AuthApiの実装タイプ</typeparam>
+        public static void ConfigureAuthApi<TImplementation>()
+                where TImplementation : class, IAuthApi
+        {
+            _serviceCollection.AddSingleton<IAuthApi, TImplementation>();
+        }
+
+        /// <summary>
+        /// AuthApi
+        /// </summary>
+        public static IAuthApi AuthApi => ServiceProvider.GetService<IAuthApi>();
+        #endregion
+    }
+}
