2505dddbc44be0d19daebc998c7d18ea647d463b
Iga <iga@entap.co.jp>
2021-04-14 15:02:11 +0900

    [update]Entap.Basic.Firebase.Auth.Line : LineAuthService : LINE認証処理実装

--- Source/Entap.Basic.Firebase.Auth.Line/Entap.Basic.Firebase.Auth.Line/LineAuthService.cs
+++ Source/Entap.Basic.Firebase.Auth.Line/Entap.Basic.Firebase.Auth.Line/LineAuthService.cs
@@ -1,18 +1,42 @@
 ﻿using System;
+using System.Net;
 using System.Threading.Tasks;
 using Entap.Basic.Auth;
+using Entap.Basic.Auth.Line;
 
 namespace Entap.Basic.Firebase.Auth.Line
 {
     public class LineAuthService : SnsAuthService, ISnsAuthService
     {
-        public LineAuthService()
+        readonly LineAuthParameter _authParameter;
+        IAuthErrorCallback _callback;
+        public LineAuthService(LineAuthParameter authParameter, IAuthErrorCallback callback = null)
         {
+            _authParameter = authParameter;
+            _callback = callback;
         }
 
-        public Task SignInAsync()
+        public async Task SignInAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var authService = new Entap.Basic.Auth.Line.LineAuthService();
+                _authParameter.AuthRequest.UpdateState();
+                var result = await authService.AuthorizeAsync(_authParameter.AuthRequest);
+                if (result.State != _authParameter.AuthRequest.State)
+                    throw new InvalidOperationException();
+                _authParameter.AccessTokenRequest.Code = result.Code;
+                var (status, token) = await authService.GetAccessTokenAsync(_authParameter.AccessTokenRequest);
+                if (status != HttpStatusCode.OK)
+                    throw new HttpListenerException((int)status);
+
+                await BasicAuthStartUp.AuthApi.PostAuthLineToken(new Api.CustomAuthToken(token.AccessToken, token.IdToken));
+            }
+            catch (Exception ex)
+            {
+                await _callback.HandleSignInErrorAsync(ex);
+                throw ex;
+            }
         }
     }
 }
