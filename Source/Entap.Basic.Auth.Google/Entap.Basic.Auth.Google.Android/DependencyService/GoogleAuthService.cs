cc158f73e84b0ac3dd1d512e776feb479693642b
Iga <iga@entap.co.jp>
2021-04-23 15:42:57 +0900

    [add]Entap.Basic.Auth.Google.Android :Googleサインイン実装
    ・PackageReference:Xamarin.GooglePlayServices.Auth追加
    ・GoogleAuthService:SingInAsync, SignOutAsyncを実装
    ・Platforms:初期化処理定義

---
+++ Source/Entap.Basic.Auth.Google/Entap.Basic.Auth.Google.Android/DependencyService/GoogleAuthService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Gms.Auth.Api.SignIn;
+using Android.Gms.Common.Apis;
+using Entap.Basic.Core.Android;
+
+namespace Entap.Basic.Auth.Google.Android
+{
+    public class GoogleAuthService
+    {
+        public GoogleAuthService()
+        {
+            if (!Platform.Initialized)
+                throw new InvalidOperationException("Please call Entap.Basic.Auth.Google.Android.Platform.Init() method.");
+        }
+
+        public async Task<GoogleSignInAccount> SingInAsync()
+        {
+            var gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DefaultSignIn)
+                .RequestIdToken(Platform.AuthClinetId)
+                .Build();
+            var client = GoogleSignIn.GetClient(Platform.Context, gso);
+
+            var result = await StarterActivity.StartForResultAsync((Activity)Platform.Context, client.SignInIntent, Platform.RequestCode);
+            var task = GoogleSignIn.GetSignedInAccountFromIntent(result.Data);
+            var account = task.GetResult(Java.Lang.Class.FromType(typeof(ApiException)));
+            return account as GoogleSignInAccount;
+        }
+
+        public Task SignOutAsync()
+        {
+            var gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DefaultSignIn)
+                .Build();
+            var client = GoogleSignIn.GetClient(Platform.Context, gso);
+
+            return client.SignOutAsync();
+        }
+    }
+}
---
+++ Source/Entap.Basic.Auth.Google/Entap.Basic.Auth.Google.Android/Platform.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+
+namespace Entap.Basic.Auth.Google.Android
+{
+    [Preserve(AllMembers = true)]
+    public static class Platform
+    {
+        internal static bool Initialized;
+
+        internal static Context Context;
+        internal static string AuthClinetId;
+        internal static int RequestCode;
+        public static void Init(Context context, string authClinetId, int requestCode)
+        {
+            if (context is null)
+                throw new ArgumentNullException(nameof(context));
+
+            if (string.IsNullOrEmpty(authClinetId))
+                throw new ArgumentNullException(nameof(authClinetId));
+
+            Context = context;
+            AuthClinetId = authClinetId;
+            RequestCode = requestCode;
+
+            Initialized = true;
+        }
+    }
+}
