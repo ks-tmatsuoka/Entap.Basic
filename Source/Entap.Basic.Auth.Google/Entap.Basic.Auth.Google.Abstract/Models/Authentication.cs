ea20ef509d3e3a677dd52612570fec0eaefe2740
Iga <iga@entap.co.jp>
2021-04-27 13:48:45 +0900

    [update]Entap.Basic.Auth.Google.Abstract : Authenticationの項目にコメント追加

--- Source/Entap.Basic.Auth.Google/Entap.Basic.Auth.Google.Abstract/Models/Authentication.cs
+++ Source/Entap.Basic.Auth.Google/Entap.Basic.Auth.Google.Abstract/Models/Authentication.cs
@@ -7,14 +7,29 @@ namespace Entap.Basic.Auth.Google
         {
         }
 
+        /// <summary>
+        /// アクセストークン（iOSのみ）
+        /// </summary>
         public string AccessToken { get; set; }
 
+        /// <summary>
+        /// アクセストークン有効期限（iOSのみ）
+        /// </summary>
         public DateTime AccessTokenExpirationDate { get; set; }
 
+        /// <summary>
+        /// IDトークン
+        /// </summary>
         public string IdToken { get; set; }
 
+        /// <summary>
+        /// IDトークン有効期限（iOSのみ）
+        /// </summary>
         public DateTime IdTokenExpirationDate { get; set; }
 
+        /// <summary>
+        /// リフレッシュトークン（iOSのみ）
+        /// </summary>
         public string RefreshToken { get; set; }
     }
 }
