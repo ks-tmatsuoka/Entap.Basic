8880857418d541b17d20d7e505321144023adb57
Iga <iga@entap.co.jp>
2021-04-27 13:39:15 +0900

    [add]Entap.Basic.Auth.Google.Abstract : Bait & Switchでのプラットフォーム処理実行
    IGoogleAuthService, AuthenticationをAbstractに移動

---
+++ Source/Entap.Basic.Auth.Google/Entap.Basic.Auth.Google.Abstract/Interfaces/DependencyService/IGoogleAuthService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Entap.Basic.Auth.Google
+{
+    public interface IGoogleAuthService
+    {
+        Task<Authentication> AuthAsync();
+        Task SignOutAsync();
+    }
+}
---
+++ Source/Entap.Basic.Auth.Google/Entap.Basic.Auth.Google.Abstract/Models/Authentication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Entap.Basic.Auth.Google
+{
+    public class Authentication
+    {
+        public Authentication()
+        {
+        }
+
+        public string AccessToken { get; set; }
+
+        public DateTime AccessTokenExpirationDate { get; set; }
+
+        public string IdToken { get; set; }
+
+        public DateTime IdTokenExpirationDate { get; set; }
+
+        public string RefreshToken { get; set; }
+    }
+}
