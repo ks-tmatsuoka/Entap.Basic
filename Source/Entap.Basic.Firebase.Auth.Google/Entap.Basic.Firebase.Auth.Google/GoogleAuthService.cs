f0fa0bbd6d2ea19d3e7860fcd37ba1418f068fab
Iga <iga@entap.co.jp>
2021-04-27 16:11:54 +0900

    [update]Entap.Basic.Firebase.Auth.Google : GoogleAuthService実装

---
+++ Source/Entap.Basic.Firebase.Auth.Google/Entap.Basic.Firebase.Auth.Google/GoogleAuthService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Entap.Basic.Auth;
+using Plugin.FirebaseAuth;
+using AuthService = Entap.Basic.Auth.Google.GoogleAuthService;
+
+namespace Entap.Basic.Firebase.Auth.Google
+{
+    public class GoogleAuthService : SnsAuthService, ISnsAuthService
+    {
+        readonly IAuthErrorCallback _errorCallback;
+        public GoogleAuthService(IAuthErrorCallback errorCallback = null)
+        {
+            _errorCallback = errorCallback;
+        }
+
+        public async Task SignInAsync()
+        {
+            try
+            {
+                var authService = new AuthService();
+                var authentication = await authService.AuthAsync();
+                var credential = CrossFirebaseAuth.Current.GoogleAuthProvider.GetCredential(authentication.IdToken+"a", authentication.AccessToken);
+                await SignInWithCredentialAsync(credential);
+            }
+            catch (Exception ex)
+            {
+                await _errorCallback.HandleSignInErrorAsync(ex);
+                throw ex;
+            }
+        }
+    }
+}
