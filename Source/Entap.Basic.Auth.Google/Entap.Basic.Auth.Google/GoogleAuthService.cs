6d8aafc98cad006e9fca692ff98125f613b871ba
Iga <iga@entap.co.jp>
2021-04-27 13:48:05 +0900

    [add]Entap.Basic.Auth.Google : GoogleAuthService : Bait & Switchの共有定義

---
+++ Source/Entap.Basic.Auth.Google/Entap.Basic.Auth.Google/GoogleAuthService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Entap.Basic.Auth.Google
+{
+    public class GoogleAuthService : IGoogleAuthService
+    {
+        public GoogleAuthService()
+        {
+        }
+
+        public Task<Authentication> AuthAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SignOutAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
