f3c199fbf4b5a09f814f0f1cebb8f64d5dec0337
Iga <iga@entap.co.jp>
2021-09-09 11:22:39 +0900

    [update]Entap.Basic.Auth.Line : WEB認証によるLINEの認証処理実装
    Bait & Switchではビルドエラーが発生したためXamarin.Fromsを追加し、DependencyServiceでiOS処理を実行
    Xamarin.Essentialsの追加、バージョン更新

---
+++ Source/Entap.Basic.Auth.Line/Entap.Basic.Auth.Line.iOS/WebAuthenticationService/WebAuthenticationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Foundation;
+using UIKit;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(Entap.Basic.Auth.Line.iOS.WebAuthenticationService))]
+namespace Entap.Basic.Auth.Line.iOS
+{
+    public class WebAuthenticationService : IWebAuthenticationService
+    {
+        public Task<WebAuthenticatorResult> AuthenticateAsync(Uri url, Uri callbaclUrl)
+            => CustomWebAuthenticator.PlatformAuthenticateAsync(
+                new WebAuthenticatorOptions { Url = url, CallbackUrl = callbaclUrl });
+
+        public static bool ContinueUserActivity(UIApplication application, NSUserActivity userActivity, UIApplicationRestorationHandler completionHandler)
+            => CustomWebAuthenticator.OpenUrl(new Uri(userActivity?.WebPageUrl?.AbsoluteString));
+    }
+}
