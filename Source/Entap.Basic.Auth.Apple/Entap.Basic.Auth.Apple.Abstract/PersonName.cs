105e6610c6c376d8295afced872c1e4d691e6095
Iga <iga@entap.co.jp>
2021-09-13 17:09:35 +0900

    [add]Entap.Basic.Auth.Apple.Abstract : PersonNameモデル定義

---
+++ Source/Entap.Basic.Auth.Apple/Entap.Basic.Auth.Apple.Abstract/PersonName.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Entap.Basic.Auth.Apple.Abstract
+{
+    /// <summary>
+    /// 個人名
+    /// https://developer.apple.com/documentation/foundation/personnamecomponents
+    /// </summary>
+    public class PersonName
+    {
+        public PersonName()
+        {
+        }
+
+        public string GivenName { get; set; }
+
+        public string FamilyName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string NamePrefix { get; set; }
+
+        public string NameSuffix { get; set; }
+
+        public string Nickname { get; set; }
+    }
+}
