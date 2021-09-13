9281347b4236bf9e2afc8a3e08684d2fb4d14c7d
Iga <iga@entap.co.jp>
2021-09-13 17:20:21 +0900

    [add]Entap.Basic.Auth.Apple.Abstract : IAppleSignInService定義

---
+++ Source/Entap.Basic.Auth.Apple/Entap.Basic.Auth.Apple.Abstract/IAppleSignInService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Entap.Basic.Auth.Apple.Abstract
+{
+    public interface IAppleSignInService
+    {
+        public Task<AppleIdCredential> SignInAsync();
+    }
+}
