3e3461f4f831e7593e43545437133783e46ffb13
Iga <iga@entap.co.jp>
2021-09-13 16:50:35 +0900

    [add]Entap.Basic.Auth.iOS : AppleSignInService定義

---
+++ Source/Entap.Basic.Auth.Apple.iOS/AppleSignInService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using AuthenticationServices;
+using Foundation;
+using UIKit;
+
+namespace Entap.Basic.Auth.Apple.iOS
+{
+    public class AppleSignInService : NSObject, IASAuthorizationControllerDelegate, IASAuthorizationControllerPresentationContextProviding
+    {
+        TaskCompletionSource<ASAuthorizationAppleIdCredential> tcsCredential;
+
+        public async Task<ASAuthorizationAppleIdCredential> GetCredential()
+        {
+            var appleIdProvider = new ASAuthorizationAppleIdProvider();
+            var request = appleIdProvider.CreateRequest();
+            request.RequestedScopes = new[] { ASAuthorizationScope.Email, ASAuthorizationScope.FullName };
+
+            var authorizationController = new ASAuthorizationController(new[] { request })
+            {
+                Delegate = this,
+                PresentationContextProvider = this
+            };
+            authorizationController.PerformRequests();
+
+            tcsCredential = new TaskCompletionSource<ASAuthorizationAppleIdCredential>();
+            var creds = await tcsCredential.Task;
+
+            return creds;
+        }
+
+        #region IASAuthorizationController Delegate
+
+        [Export("authorizationController:didCompleteWithAuthorization:")]
+        public void DidComplete(ASAuthorizationController controller, ASAuthorization authorization)
+        {
+            var creds = authorization.GetCredential<ASAuthorizationAppleIdCredential>();
+            tcsCredential?.TrySetResult(creds);
+        }
+
+        [Export("authorizationController:didCompleteWithError:")]
+        public void DidComplete(ASAuthorizationController controller, NSError error)
+        {
+            tcsCredential?.SetException(new NSErrorException(error));
+        }
+
+        #endregion
+
+        #region IASAuthorizationControllerPresentation Context Providing
+        public UIWindow GetPresentationAnchor(ASAuthorizationController controller)
+        {
+            return UIApplication.SharedApplication.KeyWindow;
+        }
+        #endregion
+    }
+}
