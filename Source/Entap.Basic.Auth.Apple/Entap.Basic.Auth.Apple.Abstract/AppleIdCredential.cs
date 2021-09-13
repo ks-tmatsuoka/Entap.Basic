7bc963751ba3fb9a59d91fae82e844f92d6a4e5a
Iga <iga@entap.co.jp>
2021-09-13 17:19:56 +0900

    [add]Entap.Basic.Auth.Apple.Abstract : AppleIdCredentialモデル定義

---
+++ Source/Entap.Basic.Auth.Apple/Entap.Basic.Auth.Apple.Abstract/AppleIdCredential.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Entap.Basic.Auth.Apple.Abstract
+{
+    /// <summary>
+    /// AppleIdCredential
+    /// https://developer.apple.com/documentation/authenticationservices/asauthorizationappleidcredential
+    /// </summary>
+    public class AppleIdCredential
+    {
+        public AppleIdCredential()
+        {
+        }
+
+        public string IdToken { get;set; }
+
+        public string Email { get;set; }
+
+        public string UserId { get; set; }
+
+        public PersonName FullName { get; set; }
+    }
+}
