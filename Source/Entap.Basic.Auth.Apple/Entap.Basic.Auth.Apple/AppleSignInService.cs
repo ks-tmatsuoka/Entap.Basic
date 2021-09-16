a984c0766629e2a5c6b433657bcc62c1030c4aa5
Iga <iga@entap.co.jp>
2021-09-16 11:06:32 +0900

    [add]Entap.Basic.Auth.Apple : AppleSignInService定義(Bait & Switchのダミー処理実装）

---
+++ Source/Entap.Basic.Auth.Apple/Entap.Basic.Auth.Apple/AppleSignInService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Entap.Basic.Auth.Apple.Abstract;
+
+namespace Entap.Basic.Auth.Apple
+{
+    public class AppleSignInService : IAppleSignInService
+    {
+        public AppleSignInService(params AuthorizationScope[] scopes)
+        {
+            var _ = scopes;
+        }
+
+        public Task<AppleIdCredential> SignInAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
