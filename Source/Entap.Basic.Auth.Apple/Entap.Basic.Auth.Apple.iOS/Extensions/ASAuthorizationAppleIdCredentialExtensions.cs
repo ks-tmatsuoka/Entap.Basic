306a85d19e80ec7bbc7a82fd57bf4159dfef497f
Iga <iga@entap.co.jp>
2021-09-16 09:17:19 +0900

    [add]Entap.Basic.Auth.Apple.iOS : Extensions : ASAuthorizationAppleIdCredentialExtensions, NSPersonNameComponentsExtensions
    Entap.Basic.Auth.Apple.Abstractの参照追加

---
+++ Source/Entap.Basic.Auth.Apple/Entap.Basic.Auth.Apple.iOS/Extensions/ASAuthorizationAppleIdCredentialExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AuthenticationServices;
+using Entap.Basic.Auth.Apple.Abstract;
+using Foundation;
+
+namespace Entap.Basic.Auth.Apple.iOS
+{
+    public static class ASAuthorizationAppleIdCredentialExtensions
+    {
+        public static AppleIdCredential ToAppleIdCredential(this ASAuthorizationAppleIdCredential credential)
+        {
+            return new AppleIdCredential()
+            {
+                Email = credential.Email,
+                IdToken = new NSString(credential.IdentityToken, NSStringEncoding.UTF8).ToString(),
+                UserId = credential.User,
+                FullName = credential.FullName?.ToPersonName(),
+            };
+        }
+    }
+}
---
+++ Source/Entap.Basic.Auth.Apple/Entap.Basic.Auth.Apple.iOS/Extensions/NSPersonNameComponentsExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Entap.Basic.Auth.Apple.Abstract;
+using Foundation;
+
+namespace Entap.Basic.Auth.Apple.iOS
+{
+    public static class NSPersonNameComponentsExtensions
+    {
+        public static PersonName ToPersonName(this NSPersonNameComponents components)
+        {
+            return new PersonName
+            {
+                FamilyName = components.FamilyName,
+                GivenName = components.GivenName,
+                MiddleName = components.MiddleName,
+                NamePrefix = components.NamePrefix,
+                NameSuffix = components.NameSuffix,
+                Nickname = components.Nickname,
+            };
+        }
+    }
+}
