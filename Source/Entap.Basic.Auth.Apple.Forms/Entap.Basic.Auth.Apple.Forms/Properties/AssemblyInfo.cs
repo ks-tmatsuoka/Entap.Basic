675d0d66be8a9027f772a32422e0fe97d84ba277
Iga <iga@entap.co.jp>
2021-09-29 15:44:21 +0900

    [add]Entap.Basic.Auth.Apple.Forms : Entap.Basic.Auth.Apple.Forms.NuGetプロジェクト追加

---
+++ Source/Entap.Basic.Auth.Apple.Forms/Entap.Basic.Auth.Apple.Forms/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+[assembly: XmlnsDefinition("http://entap.co.jp/schemas/2021/basic", "Entap.Basic.Auth.Apple.Forms")]
