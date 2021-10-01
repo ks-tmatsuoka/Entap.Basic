9fe4585c83c3d018d36c2a2013f60c2374cd3f60
Iga <iga@entap.co.jp>
2021-10-01 16:38:31 +0900

    [update]Entap.Basic.Firebase.Auth.Apple : AppleAuthService定義

---
+++ Source/Entap.Basic.Firebase.Auth.Apple/Entap.Basic.Firebase.Auth.Apple/AppleAuthService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Entap.Basic.Auth;
+using Entap.Basic.Auth.Apple;
+using Entap.Basic.Auth.Apple.Abstract;
+using Plugin.FirebaseAuth;
+
+namespace Entap.Basic.Firebase.Auth.Apple
+{
+    public class AppleAuthService : SnsAuthService, ISnsAuthService
+    {
+        const string ProviderId = "apple.com";
+        readonly IAuthErrorCallback _errorCallback;
+        AuthorizationScope[] _scopes;
+        public AppleAuthService(IAuthErrorCallback errorCallback = null, params AuthorizationScope[] scopes)
+        {
+            _errorCallback = errorCallback;
+            _scopes = scopes;
+        }
+
+        public async Task SignInAsync()
+        {
+            try
+            {
+                var service = new AppleSignInService(_scopes);
+                var id = await service.SignInAsync();
+                var credential = CrossFirebaseAuth.Current.OAuthProvider.GetCredential(ProviderId, id.IdToken);
+
+                await CrossFirebaseAuth.Current.Instance.SignInWithCredentialAsync(credential);
+            }
+            catch (Exception ex)
+            {
+                await _errorCallback.HandleSignInErrorAsync(ex);
+                throw ex;
+            }
+        }
+
+        public Task SignOutAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
