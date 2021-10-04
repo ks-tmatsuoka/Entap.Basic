683397fe535a7458d3520243b374e73dd888440d
Iga <iga@entap.co.jp>
2021-10-04 13:16:51 +0900

    [add]Entap.Basic : Basic : 初期化処理追加

---
+++ Source/Entap.Basic/Entap.Basic/Basic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Entap.Basic
+{
+    public class Basic
+    {
+        public static void Init()
+        {
+            Entap.Basic.Auth.Apple.Forms.Controls.Init();
+        }
+    }
+}
