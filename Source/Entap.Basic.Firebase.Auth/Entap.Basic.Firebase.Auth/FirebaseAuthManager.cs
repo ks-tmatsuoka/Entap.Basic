63be211e25996f1361d81ee583bcd6687f1a34ac
Iga <iga@entap.co.jp>
2021-11-17 13:06:56 +0900

    [add]Entap.Basic.Firebase.Auth : FirebaseAuthManager

---
+++ Source/Entap.Basic.Firebase.Auth/Entap.Basic.Firebase.Auth/FirebaseAuthManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using Entap.Basic.Auth;
+using Entap.Basic.Auth.Abstractions;
+using Plugin.FirebaseAuth;
+
+namespace Entap.Basic.Firebase.Auth
+{
+    public class FirebaseAuthManager : IAuthManager
+    {
+        readonly IAuthErrorCallback _authErrorCallback;
+        IUserDataRepository UserDataRepository => BasicFirebaseAuthStartUp.UserDataRepository;
+        public FirebaseAuthManager(IAuthErrorCallback authErrorCallback = null)
+        {
+            _authErrorCallback = authErrorCallback;
+        }
+
+        public bool IsSignedIn => AuthHelper.IsSignedIn;
+
+        public bool IsPasswordAuthSupported => PasswordAuthService is not null;
+        public IPasswordAuthService PasswordAuthService => BasicFirebaseAuthStartUp.PasswordAuthService;
+
+        public bool IsTwitterAuthSupported => TwitterAuthService is not null;
+        public ITwitterAuthService TwitterAuthService => BasicFirebaseAuthStartUp.TwitterAuthService;
+
+        public bool IsFacebookAuthSupported => FacebookAuthService is not null;
+        public IFacebookAuthService FacebookAuthService => BasicFirebaseAuthStartUp.FacebookAuthService;
+
+        public bool IsLineAuthSupported => LineAuthService is not null;
+        public ILineAuthService LineAuthService => BasicFirebaseAuthStartUp.LineAuthService;
+
+        public bool IsGoogleAuthSupported => GoogleAuthService is not null;
+        public IGoogleAuthService GoogleAuthService => BasicFirebaseAuthStartUp.GoogleAuthService;
+
+        public bool IsAppleAuthSupported => AppleAuthService is not null;
+        public IAppleAuthService AppleAuthService => BasicFirebaseAuthStartUp.AppleAuthService;
+
+        public virtual Task SignOutAsync()
+        {
+            if (!IsSignedIn)
+                throw new InvalidOperationException();
+
+            try
+            { 
+                CrossFirebaseAuth.Current.Instance.SignOut();
+                UserDataRepository.RemoveAccessToken();
+            }
+            catch (Exception ex)
+            {
+                _authErrorCallback?.HandleSignOutErrorAsync(ex);
+                throw ex;
+            }
+            return Task.CompletedTask;
+        }
+
+        public virtual async Task RefreshServerTokenAsync()
+            => await AuthHelper.StoreServerAccessTokenAsync();
+    }
+}
