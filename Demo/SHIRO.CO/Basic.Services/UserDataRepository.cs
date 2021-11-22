5560f1912ac24573c0f30c86b02373aaf4d04aa4
Iga <iga@entap.co.jp>
2021-11-22 11:52:52 +0900

    [add]Demo : UserDataRepository

---
+++ Demo/SHIRO.CO/Basic.Services/UserDataRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SHIRO.CO
+{
+    public class UserDataRepository : Entap.Basic.Firebase.Auth.UserDataRepository
+    {
+        public UserDataRepository() : base(new SecureStorageManager())
+        {
+
+        }
+
+        public override Task SetAccessTokenAsync(string accessToken)
+        {
+            BasicApiManager.Current.SetAuthorization(accessToken);
+            return base.SetAccessTokenAsync(accessToken);
+        }
+
+        public override void RemoveAccessToken()
+        {
+            BasicApiManager.Current.ClearAuthorization();
+            base.RemoveAccessToken();
+        }
+    }
+}
