8d18e11de7af9f021bd4ae6fb4cd5c5843bae759
seki <seki@seki-MacBook-Pro.local>
2021-12-07 13:19:37 +0900

    [update] DisplaySizeManager summary追記

--- Source/Entap.Basic.Forms/Entap.Basic.Forms/DisplaySizeManager/DisplaySizeManager.cs
+++ Source/Entap.Basic.Forms/Entap.Basic.Forms/DisplaySizeManager/DisplaySizeManager.cs
@@ -4,20 +4,17 @@ using Xamarin.Forms;
 
 namespace Entap.Basic.Forms
 {
+    /// <summary>
+    /// <para>注意</para>
+    /// <para>iOSでナビゲーションバーない場合について</para>
+    /// <para>TopNavigationHeightとPageSizeとiOSDisplaySizeRecivedのNavigationBarHeight、PageHeightは、HasNavigationBar="False"でナビゲーション使ってない場合でもナビゲーションバーがある際のサイズを返すので、ナビゲーションバーのないページでページのサイズ取得したい場合PageSizeのHeightにTopNavigationHeightを足した数値が正確なPageSizeとなります。PageManager.SetMainPageでナビゲーション使ってない場合はPageSizeのHeightにTopNavigationHeightを足す必要はないです。 </para>
+    /// <para>Androidでナビゲーションバーない場合について</para>
+    /// <para>TopNavigationHeightとPageSizeとiOSDisplaySizeRecivedのNavigationBarHeight、PageHeightは、HasNavigationBar="False"およびSetMainPageでナビゲーション使ってない場合でもナビゲーションバーがある際のサイズを返すので、ナビゲーションバーのないページでページのサイズ取得したい場合PageSizeのHeightにTopNavigationHeightを足した数値が正確なPageSizeとなります。</para>
+    /// <para>iOS Androidともにナビゲーションバーがない場合のPageの高さはScreenSize - StatusBarHeight で取得できます。</para>
+    /// <para>TopNavigationHeightとPageSizeとiOSDisplaySizeRecivedのNavigationBarHeight、PageHeightは、HasNavigationBar="False"およびSetMainPageでナビゲーション使ってない場合でもナビゲーションバーがある際のサイズを返すので、ナビゲーションバーのないページでページのサイズ取得したい場合PageSizeのHeightにTopNavigationHeightを足した数値が正確なPageSizeとなります。</para>
+    /// </summary>
     public class DisplaySizeManager
     {
-        // 注意
-        // iOSでナビゲーションバーない場合について
-        // TopNavigationHeightとPageSizeとiOSDisplaySizeRecivedのNavigationBarHeight、PageHeightは、
-        // HasNavigationBar="False"でナビゲーション使ってない場合でもナビゲーションバーがある際のサイズを返すので、
-        // ナビゲーションバーのないページでページのサイズ取得したい場合PageSizeのHeightにTopNavigationHeightを足した数値が正確なPageSizeとなります。
-        //　PageManager.SetMainPageでナビゲーション使ってない場合はPageSizeのHeightにTopNavigationHeightを足す必要はないです。
-        //
-        // androidでナビゲーションバーない場合について
-        // TopNavigationHeightとPageSizeとiOSDisplaySizeRecivedのNavigationBarHeight、PageHeightは、
-        // HasNavigationBar="False"およびSetMainPageでナビゲーション使ってない場合でもナビゲーションバーがある際のサイズを返すので、
-        // ナビゲーションバーのないページでページのサイズ取得したい場合PageSizeのHeightにTopNavigationHeightを足した数値が正確なPageSizeとなります。
-
         static IGetDisplaySize getGetDisplaySize;
         static IGetDisplaySize getGetDisplaySizeInstanse
         {
@@ -28,6 +25,10 @@ namespace Entap.Basic.Forms
                 return getGetDisplaySize;
             }
         }
+
+        /// <summary>
+        /// 画面のサイズ(ナビゲーションバーやステータスバーの高さを含む)
+        /// </summary>
         public static Size ScreenSize
         {
             get
@@ -36,7 +37,11 @@ namespace Entap.Basic.Forms
             }
         }
 
-        // iOSの場合、各ページのコードビハインドやViewModelのコンストラクタ内では使用せず、iOSDisplaySizeRecivedの方を使う
+        /// <summary>
+        /// <para>ページのサイズ(ナビゲーションバーやステータスバーの高さは含まない)</para>
+        /// <para>注意</para>
+        /// <para>iOSの場合、各ページのコードビハインドやViewModelのコンストラクタ内では使用せず、iOSDisplaySizeChangedの方を使う</para>
+        /// </summary>
         public static Size PageSize
         {
             get
@@ -61,7 +66,10 @@ namespace Entap.Basic.Forms
             }
         }
 
-        // iOSの場合、各ページのコードビハインドやViewModelのコンストラクタ内でナビゲーションバーなどの高さが欲しければこれを使用する
+        /// <summary>
+        /// <para>注意</para>
+        /// <para>iOSの場合、各ページのコードビハインドやViewModelのコンストラクタ内でナビゲーションバーなどの高さが欲しければこれを使用する</para>
+        /// </summary>
         public static event EventHandler<iOSDisplaySizeRecivedEventArgs> iOSDisplaySizeChanged;
         [EditorBrowsable(EditorBrowsableState.Never)]
         public static void OniOSDisplaySizeChanging(object obj, iOSDisplaySizeRecivedEventArgs args)
@@ -81,7 +89,10 @@ namespace Entap.Basic.Forms
             }
         }
 
-        // iOSでは各ページのコードビハインドやViewModelのコンストラクタ内では使用せず、iOSDisplaySizeRecivedの方を使う
+        /// <summary>
+        /// <para>注意</para>
+        /// <para>iOSの場合、各ページのコードビハインドやViewModelのコンストラクタ内では使用せず、iOSDisplaySizeChangedの方を使う</para>
+        /// </summary>
         public static double TopNavigationHeight
         {
             get
