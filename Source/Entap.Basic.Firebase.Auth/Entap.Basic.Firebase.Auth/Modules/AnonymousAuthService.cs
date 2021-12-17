21caf412d3f18db33914c5f3ab545fc87a076d83
Iga <iga@entap.co.jp>
2021-12-17 11:12:54 +0900

    [add]Entap.Basic.Firebase.Auth : AnonymousAuthService

---
+++ Source/Entap.Basic.Firebase.Auth/Entap.Basic.Firebase.Auth/Modules/AnonymousAuthService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Entap.Basic.Auth;
+using Plugin.FirebaseAuth;
+
+namespace Entap.Basic.Firebase.Auth
+{
+    public class AnonymousAuthService : IAnonymousAuthService
+    {
+        private readonly IAuthErrorCallback _errorCallback;
+
+        public AnonymousAuthService(IAuthErrorCallback errorCallback)
+        {
+            _errorCallback = errorCallback;
+        }
+
+        public async Task SignInAsync()
+        {
+            try
+            {
+                var result = await CrossFirebaseAuth.Current.Instance.SignInAnonymouslyAsync();
+                await AuthHelper.StoreServerAccessTokenAsync();
+            }
+            catch (Exception ex)
+            {
+                AuthHelper.TrySignOut();
+                await _errorCallback?.HandleSignInErrorAsync(ex);
+                throw;
+            }
+        }
+    }
+}
