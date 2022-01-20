68d4de60f1f42d9b8af37dd81ee7e5372c21a6ef
Iga <iga@entap.co.jp>
2022-01-20 11:51:14 +0900

    [update]Entap.Basic.Line.Auth : LineAuthService.ios : LINEログイン組み込み処理定義
    https://developers.line.biz/ja/docs/ios-sdk/swift/integrate-line-login/

--- Source/Entap.Basic.Auth.Line/Entap.Basic.Auth.Line/LineAuthService.ios.cs
+++ Source/Entap.Basic.Auth.Line/Entap.Basic.Auth.Line/LineAuthService.ios.cs
@@ -1,10 +1,47 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Foundation;
+using LineSDKObjC;
+using UIKit;
 
 namespace Entap.Basic.Auth.Line
 {
     public partial class LineAuthService : ILineAuthService
     {
+        /// <summary>
+        /// 初期化
+        /// https://developers.line.biz/ja/docs/ios-sdk/swift/integrate-line-login/#call-loginmanagersetup-method
+        /// </summary>
+#nullable enable
+        public static void Init(string channelId, string? universalLinkUrl)
+#nullable disable
+        {
+            LineSDKLoginManager.SharedManager.SetupWithChannelID(channelId, new NSUrl(universalLinkUrl));
+        }
+
+        /// <summary>
+        /// ContinueUserActivity
+        /// https://developers.line.biz/ja/docs/ios-sdk/swift/universal-links-support/#modify-app-delegate
+        /// </summary>
+        public static bool ContinueUserActivity(UIApplication application, NSUserActivity userActivity, UIApplicationRestorationHandler completionHandler)
+            => LineSDKObjC.LineSDKLoginManager.SharedManager.Application(application, userActivity.WebPageUrl, new NSDictionary<NSString, NSObject>());
+
+        /// <summary>
+        /// OpenUrl
+        /// https://developers.line.biz/ja/docs/ios-sdk/swift/integrate-line-login/#modify-app-delegate
+        /// </summary>
+        public static bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+            => LineSDKObjC.LineSDKLoginManager.SharedManager.Application(app, url, new NSDictionary<NSString, NSObject>());
+
+        /// <summary>
+        /// OpenUrlContexts
+        /// https://developers.line.biz/ja/docs/ios-sdk/swift/integrate-line-login/#modify-scene-delegates
+        /// </summary>
+        public static void OpenUrlContexts(UIScene scene, NSSet<UIOpenUrlContext> urlContexts)
+            => LineSDKObjC.LineSDKLoginManager.SharedManager.Application(UIApplication.SharedApplication, url: urlContexts?.ToArray()?.FirstOrDefault()?.Url, new NSDictionary<NSString, NSObject>());
+
+
         public Task<LoginResult> PlatformLoginAsync(params LoginScope[] scopes)
         {
             throw new NotImplementedException();
