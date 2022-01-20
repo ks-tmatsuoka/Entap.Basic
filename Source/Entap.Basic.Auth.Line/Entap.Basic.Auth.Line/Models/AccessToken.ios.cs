4ce69281daf680ddc69384208cb3ae5e6cc2841f
Iga <iga@entap.co.jp>
2022-01-20 15:26:56 +0900

    [add]Entap.Basic.Auth.Line : AccessTokenモデル定義

---
+++ Source/Entap.Basic.Auth.Line/Entap.Basic.Auth.Line/Models/AccessToken.ios.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Entap.Basic.Auth.Line
+{
+    public partial class AccessToken
+    {
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonProperty("scope")]
+        public string Scope { get; set; }
+
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
+
+        [JsonProperty("createdAt")]
+        public double CreatedAt { get; set; }
+
+        [JsonProperty("id_token")]
+        public string IdToken { get; set; }
+
+        [JsonProperty("access_token")]
+        public string AccessTokenAccessToken { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+    }
+}
