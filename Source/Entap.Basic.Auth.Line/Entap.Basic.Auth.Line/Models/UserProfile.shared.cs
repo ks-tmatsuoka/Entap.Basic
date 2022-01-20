edbe1febee720437f92a7bbfbab31e3e1190684d
Iga <iga@entap.co.jp>
2022-01-20 10:36:02 +0900

    [add]Entap.Basic.Auth.Line : UserProfile定義

--- Source/Entap.Basic.Auth.Line/Entap.Basic.Auth.Line/Models/UserProfile.shared.cs
+++ Source/Entap.Basic.Auth.Line/Entap.Basic.Auth.Line/Models/UserProfile.shared.cs
@@ -1,10 +1,23 @@
 ﻿using System;
-namespace Entap.Basic.Auth.Line.Models
+namespace Entap.Basic.Auth.Line
 {
+    /// <summary>
+	/// ユーザープロフィール
+    /// iOS:https://github.com/line/line-sdk-ios-swift/blob/master/LineSDK/LineSDK/Login/Model/UserProfile.swift
+    /// Android:https://github.com/line/line-sdk-android/blob/master/line-sdk/src/main/java/com/linecorp/linesdk/LineProfile.java
+	/// </summary>
     public class UserProfile
     {
         public UserProfile()
         {
         }
+
+        public string UserId { get; internal set; }
+
+        public string DisplayName { get; internal set; }
+
+        public Uri PictureURL { get; internal set; }
+
+        public string StatusMessage { get; internal set; }
     }
 }
