97d4c75f34451be9e77f05caf1e4bc36ada6d44a
Iga <iga@entap.co.jp>
2022-01-24 08:06:32 +0900

    [update]Entap.Basic.Auth.Line : ログイン結果を保有するプロパティ定義

--- Source/Entap.Basic.Auth.Line/Entap.Basic.Auth.Line/Models/LoginResult.shared.cs
+++ Source/Entap.Basic.Auth.Line/Entap.Basic.Auth.Line/Models/LoginResult.shared.cs
@@ -7,7 +7,17 @@ namespace Entap.Basic.Auth.Line
         {
         }
 
-        public LineAccessTokenResponse LineAccessToken { get; internal set; }
+        public bool IsCanceled { get; internal set; }
+
+#nullable enable
+        public Exception? Exception { get; internal set; }
+#nullable disable
+
+        public bool IsFaulted => Exception is not null;
+
+#nullable enable
+        public LineAccessTokenResponse? LineAccessToken { get; internal set; }
+#nullable disable
 
 #nullable enable
         public UserProfile? UserProfile { get; internal set; }
