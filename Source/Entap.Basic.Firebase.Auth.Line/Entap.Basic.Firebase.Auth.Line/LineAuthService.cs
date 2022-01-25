7cab0638af20c6d9000f8d017daa91767453fbe1
Iga <iga@entap.co.jp>
2022-01-25 09:20:24 +0900

    [update]Entap.Basic.Firebase.Auth.Line : LineAuthService
    ・LoginScopeの設定処理追加
    ・LoginResultを引数にしたサインイン処理追加

--- Source/Entap.Basic.Firebase.Auth.Line/Entap.Basic.Firebase.Auth.Line/LineAuthService.cs
+++ Source/Entap.Basic.Firebase.Auth.Line/Entap.Basic.Firebase.Auth.Line/LineAuthService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Linq;
 using System.Threading.Tasks;
 using Entap.Basic.Auth;
 using Entap.Basic.Auth.Line;
@@ -15,13 +16,43 @@ namespace Entap.Basic.Firebase.Auth.Line
             _callback = callback;
         }
 
+        static LoginScope[] _loginScopes;
+        public static void SetLoginScopes(params LoginScope[] scopes)
+        {
+            _loginScopes = scopes;
+        }
+
         public async Task SignInAsync()
         {
+            if (_loginScopes?.Any() != true)
+                throw new InvalidOperationException($"Please call {nameof(LineAuthService.SetLoginScopes)} method.");
+
             try
             {
                 var authService = new Entap.Basic.Auth.Line.LineAuthService();
-                var token = await authService.LoginAsync();
-                var customToken = new Api.CustomAuthToken(token.AccessToken, token.IdToken);
+                var result = await authService.LoginAsync(_loginScopes);
+                await SignInAsync(result);
+            }
+            catch (Exception ex)
+            {
+                AuthHelper.TrySignOut();
+                BasicFirebaseAuthStartUp.UserDataRepository.RemoveAccessToken();
+                await _callback.HandleSignInErrorAsync(ex);
+                throw;
+            }
+        }
+
+        public async Task SignInAsync(LoginResult result)
+        {
+            try
+            {
+                if (result.IsCanceled)
+                    throw new TaskCanceledException();
+
+                if (result.IsFaulted)
+                    throw result.Exception;
+
+                var customToken = new Api.CustomAuthToken(result.LineAccessToken.AccessToken, result.LineAccessToken.IdToken);
                 var serverToken = await BasicFirebaseAuthStartUp.AuthApi.PostAuthLineToken(customToken);
                 await BasicFirebaseAuthStartUp.UserDataRepository.SetAccessTokenAsync(serverToken);
                 var firebaseCustomToken = await BasicFirebaseAuthStartUp.AuthApi.PostAuthFirebaseCustomToken();
@@ -34,7 +65,7 @@ namespace Entap.Basic.Firebase.Auth.Line
                 AuthHelper.TrySignOut();
                 BasicFirebaseAuthStartUp.UserDataRepository.RemoveAccessToken();
                 await _callback.HandleSignInErrorAsync(ex);
-                throw ex;
+                throw;
             }
         }
     }
