3771af5fc2ae232c83c68310e66d30312cdb6542
Iga <iga@entap.co.jp>
2022-02-01 09:17:44 +0900

    [add]Entap.Basic.Core : BasicStartup : DIサービスの登録処理追加

---
+++ Source/Entap.Basic.Core/Entap.Basic.Core/Core/BasicStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Entap.Basic.Core
+{
+    public class BasicStartup
+    {
+        public ServiceCollection ServiceCollection;
+        public IServiceProvider ServiceProvider => ServiceCollection.BuildServiceProvider();
+
+        static readonly Lazy<BasicStartup> _instance = new Lazy<BasicStartup>(() => new BasicStartup());
+        public static BasicStartup Current => _instance.Value;
+
+        public BasicStartup()
+        {
+            ServiceCollection = new ServiceCollection();
+        }
+    }
+}
+
+
