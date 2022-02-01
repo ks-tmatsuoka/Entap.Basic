d9fa4f5384124aec46aa02d11cea56cc30d1a21a
Iga <iga@entap.co.jp>
2022-02-01 09:23:03 +0900

    [update]Entap.Basic.Core.Android : ContextExtensions : ForegroundService起動処理定義

---
+++ Source/Entap.Basic.Core/Entap.Basic.Core.Android/Extensions/ContextExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace Entap.Basic.Core.Android
+{
+    public static class ContextExtensions
+    {
+        /// <summary>
+        /// ForegroundServiceを起動する
+        /// https://docs.microsoft.com/ja-jp/xamarin/android/app-fundamentals/services/foreground-services
+        /// </summary>
+        public static void StartForegroundServiceCompat<T>(this Context context, Bundle args = null) where T : Service
+        {
+            var intent = new Intent(context, typeof(T));
+            if (args != null)
+            {
+                intent.PutExtras(args);
+            }
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+            {
+                context.StartForegroundService(intent);
+            }
+            else
+            {
+                context.StartService(intent);
+            }
+        }
+    }
+}
