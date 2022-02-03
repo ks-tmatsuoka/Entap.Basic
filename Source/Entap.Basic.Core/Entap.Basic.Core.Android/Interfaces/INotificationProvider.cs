0c79c795b10dac87cc3d7676dc49894d8f31e99c
Iga <iga@entap.co.jp>
2022-02-03 11:56:50 +0900

    [add]Entap.Basic.Core.Android : INotificationProvider追加

---
+++ Source/Entap.Basic.Core/Entap.Basic.Core.Android/Interfaces/INotificationProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Android.App;
+
+namespace Entap.Basic.Core.Android
+{
+    /// <summary>
+    /// 通知プロバイダー
+    /// </summary>
+    public interface INotificationProvider
+    {
+        /// <summary>
+        /// 通知ID
+        /// </summary>
+        int NotificationId { get; }
+
+        /// <summary>
+        /// 通知チャネル生成処理
+        /// https://developer.android.com/training/notify-user/channels?hl=ja#CreateChannel
+        /// </summary>
+        void CreateNotificationChannel();
+
+        /// <summary>
+        /// 通知
+        /// https://developer.android.com/training/notify-user/build-notification?hl=ja
+        /// </summary>
+        Notification Notification { get; }
+    }
+}
